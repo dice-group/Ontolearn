567997d2f1c8b4761b690eaaa8a28f3aecf05409
Demirrr <caglardemir8@gmail.com>
2020-05-26 09:46:16 +0200

    New examples and tests are added.

--- tests/test_util.py
+++ tests/test_util.py
@@ -5,9 +5,8 @@ from core.base import KnowledgeBase
 
 
 def test_serializer_deserializer():
-    path_of_example_kb = 'data/family-benchmark_rich_background.owl'
-    kb = KnowledgeBase(path_of_example_kb)
+    m=list(range(10))
 
-    serializer(object_=list(kb.concepts.keys()), path='.', serialized_name='concepts')
+    serializer(object_=m, path='.', serialized_name='concepts')
     temp = deserializer(path='.', serialized_name='concepts')
-    assert list(kb.concepts.keys()) == temp
+    assert m == temp
