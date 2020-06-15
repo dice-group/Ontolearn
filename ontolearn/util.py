4ccf6c4d6de084f5ffca6c54115b7394c2c8760a
Demirrr <caglardemir8@gmail.com>
2020-06-15 14:45:15 +0200

    torch dependecy removed.

--- ontolearn/util.py
+++ ontolearn/util.py
@@ -156,20 +156,3 @@ def create_logger(*, name, p):
     logger.addHandler(fh)
 
     return logger
-
-"""
-class TorchData(Dataset):
-    def __init__(self, X: torch.Tensor, y: torch.Tensor):
-        self.X = X
-        self.y = y
-    def __len__(self):
-        return len(self.X)
-
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-
-        x = self.X[idx]
-        y = self.y[idx]
-        return x, y
-"""
