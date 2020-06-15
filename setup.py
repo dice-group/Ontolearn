57089976c0bf465ff2de8b44b4d7330e17ed35af
Demirrr <caglardemir8@gmail.com>
2020-06-15 12:18:08 +0200

    descriptions are updated.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-with open('README.md','r') as fh:
-    long_description=fh.read()
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
 
 setup(
     name='ontolearn',
@@ -9,14 +9,15 @@ setup(
                 'includes modules for processing knowledge bases, inductive logic programming and ontology. '
                 'engineering.',
     version='0.0.1',
-    packages=find_packages(exclude=('tests', 'test.*','examples.*')),
+    packages=find_packages(exclude=('tests', 'test.*', 'examples.*')),
     install_requires=['scikit-learn>=0.22.1',
                       'owlready2>=0.23'],
     author='Caglar Demir',
     author_email='caglardemir8@gmail.com',
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License", ],
     python_requires='>=3.6',
     long_description=long_description,
+    long_description_content_type="text/markdown",
 )
