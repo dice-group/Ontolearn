28b29f5d967d1247c03db81c7b66128d60895826
Demirrr <caglardemir8@gmail.com>
2020-06-24 11:08:14 +0200

    variable name changed for readability.

--- examples/refinements.py
+++ examples/refinements.py
@@ -1,6 +1,5 @@
 from ontolearn import KnowledgeBase, CustomRefinementOperator
-
 kb = KnowledgeBase(path='../data/family-benchmark_rich_background.owl')
 rho = CustomRefinementOperator(kb)
-for refs in enumerate(rho.refine(kb.thing)):
-    print(refs)
+for r in rho.refine(kb.thing):
+    print(r)
