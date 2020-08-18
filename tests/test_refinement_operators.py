05c02d4d6bc76c445a36803049db198988166e71
Stefan Heindorf <heindorf@uni-paderborn.de>
2020-08-18 15:57:25 +0200

    avoid flake8 warnings in tests

--- tests/test_refinement_operators.py
+++ tests/test_refinement_operators.py
@@ -1,8 +1,9 @@
 """ Test for refinement_operators.py"""
 from ontolearn import KnowledgeBase, CustomRefinementOperator
 
+
 def test_refinement_operator():
     kb = KnowledgeBase(path='data/family-benchmark_rich_background.owl')
     rho = CustomRefinementOperator(kb)
-    for refs in enumerate(rho.refine(kb.thing)):
+    for _ in enumerate(rho.refine(kb.thing)):
         pass
