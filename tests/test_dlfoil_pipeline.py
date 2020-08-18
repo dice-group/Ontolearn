a08c53b204cec16601f3b277a5e70b76b5e39d43
Stefan Heindorf <heindorf@uni-paderborn.de>
2020-08-18 15:57:25 +0200

    avoid flake8 warnings in tests

--- tests/test_dlfoil_pipeline.py
+++ tests/test_dlfoil_pipeline.py
@@ -1,28 +1,35 @@
 """ Test the default pipeline for structured machine learning"""
 
-from ontolearn import *
 import json
 
+from ontolearn import CustomConceptLearner
+from ontolearn import CustomRefinementOperator
+from ontolearn import DLFOILHeuristic
+from ontolearn import F1
+from ontolearn import KnowledgeBase
+from ontolearn import SearchTree
 
 
 def test_dfoil():
     with open('examples/synthetic_problems.json') as json_file:
         settings = json.load(json_file)
 
-    kb = KnowledgeBase(path=settings['data_path'][3:]) # because '../data/family-benchmark_rich_background.owl'
+    # because '../data/family-benchmark_rich_background.owl'
+    kb = KnowledgeBase(path=settings['data_path'][3:])
 
     for str_target_concept, examples in settings['problems'].items():
         p = set(examples['positive_examples'])
         n = set(examples['negative_examples'])
         print('Target concept: ', str_target_concept)
-        model = CustomConceptLearner(knowledge_base=kb,
-                                     refinement_operator=CustomRefinementOperator(kb=kb),
-                                     quality_func=F1(),
-                                     heuristic_func=DLFOILHeuristic(),
-                                     search_tree=SearchTree(),
-                                     terminate_on_goal=True,
-                                     iter_bound=1_00,
-                                     verbose=True)
+        model = CustomConceptLearner(
+            knowledge_base=kb,
+            refinement_operator=CustomRefinementOperator(kb=kb),
+            quality_func=F1(),
+            heuristic_func=DLFOILHeuristic(),
+            search_tree=SearchTree(),
+            terminate_on_goal=True,
+            iter_bound=1_00,
+            verbose=True)
 
         model.predict(pos=p, neg=n)
         model.show_best_predictions(top_n=10_000, key='quality')
--- tests/test_length_based_learner.py
+++ tests/test_length_based_learner.py
@@ -1,14 +1,21 @@
 """ Test the default pipeline for structured machine learning"""
 
-from ontolearn import *
 import json
+
+from ontolearn import CELOEHeuristic
+from ontolearn import F1
+from ontolearn import KnowledgeBase
+from ontolearn import LengthBasedRefinement
+from ontolearn import LengthBaseLearner
+from ontolearn import SearchTreePriorityQueue
 
 
 def test_lengthbasedlearner():
     with open('examples/synthetic_problems.json') as json_file:
         settings = json.load(json_file)
 
-    kb = KnowledgeBase(path=settings['data_path'][3:])  # because '../data/family-benchmark_rich_background.owl'
+    # because '../data/family-benchmark_rich_background.owl'
+    kb = KnowledgeBase(path=settings['data_path'][3:])
     for str_target_concept, examples in settings['problems'].items():
         p = set(examples['positive_examples'])
         n = set(examples['negative_examples'])
@@ -16,18 +23,20 @@ def test_lengthbasedlearner():
         concepts_to_ignore = set()
         # lets inject more background info
         if str_target_concept in ['Granddaughter', 'Aunt', 'Sister']:
-            concepts_to_ignore.update({'Brother', 'Father', 'Uncle', 'Grandparent'})
-            model = LengthBaseLearner(knowledge_base=kb,
-                                      refinement_operator=LengthBasedRefinement(kb=kb),
-                                      quality_func=F1(),
-                                      min_length=1,  # think better variable name
-                                      heuristic_func=CELOEHeuristic(),
-                                      search_tree=SearchTreePriorityQueue(),
-                                      terminate_on_goal=True,
-                                      iter_bound=1_000,
-                                      max_num_of_concepts_tested=5_000,
-                                      ignored_concepts=concepts_to_ignore,
-                                      verbose=True)
+            concepts_to_ignore.update(
+                {'Brother', 'Father', 'Uncle', 'Grandparent'})
+            model = LengthBaseLearner(
+                knowledge_base=kb,
+                refinement_operator=LengthBasedRefinement(kb=kb),
+                quality_func=F1(),
+                min_length=1,
+                heuristic_func=CELOEHeuristic(),
+                search_tree=SearchTreePriorityQueue(),
+                terminate_on_goal=True,
+                iter_bound=1_000,
+                max_num_of_concepts_tested=5_000,
+                ignored_concepts=concepts_to_ignore,
+                verbose=True)
 
             preds = model.predict(pos=p, neg=n, n=10)
-            assert len(preds)
+            assert len(preds) > 0
