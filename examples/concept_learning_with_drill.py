4647d4d85b91d8186317e474e341e4384df508c2
Caglar Demir <caglardemir8@gmail.com>
2020-09-16 14:55:40 +0200

    example of drill.

--- examples/concept_learning_with_drill.py
+++ examples/concept_learning_with_drill.py
@@ -1,5 +1,6 @@
-from ontolearn import KnowledgeBase, LengthBasedRefinement, SearchTreePriorityQueue, LearningProblemGenerator
-from ontolearn.rl import DrillTrainer
+from ontolearn import KnowledgeBase, LengthBasedRefinement, SearchTreePriorityQueue, LearningProblemGenerator, \
+    LengthBaseLearner
+from ontolearn.rl import DrillTrainer, DrillHeuristic, DrillConceptLearner
 from ontolearn.metrics import F1
 from ontolearn.heuristics import Reward
 import json
@@ -15,20 +16,43 @@ kb = KnowledgeBase(path=settings['data_path'])
 rho = LengthBasedRefinement(kb=kb)
 lp_gen = LearningProblemGenerator(knowledge_base=kb,
                                   refinement_operator=rho,
-                                  num_problems=200, depth=1, min_length=1)
+                                  num_problems=3, depth=1, min_length=1)
 
 instance_emb = pd.read_csv('../embeddings/instance_emb.csv', index_col=0)
 # apply_TSNE_on_df(instance_emb)
-
 trainer = DrillTrainer(
     knowledge_base=kb,
     refinement_operator=rho,
     quality_func=F1(),
     reward_func=Reward(),
     search_tree=SearchTreePriorityQueue(),
-    train_data=settings['problems'],
     pre_trained_drill=None,
-    relearn_rate_per_problem=3.0,
     learning_problem_generator=lp_gen,
-    instance_embeddings=instance_emb)
+    instance_embeddings=instance_emb,
+    verbose=False)
 trainer.start()
+
+for str_target_concept, examples in settings['problems'].items():
+    p = set(examples['positive_examples'])
+    n = set(examples['negative_examples'])
+    print('Target concept: ', str_target_concept)
+    concepts_to_ignore = set()
+    # lets inject more background info
+    if str_target_concept in ['Granddaughter', 'Aunt', 'Sister']:
+        concepts_to_ignore.update({'Brother', 'Father', 'Uncle', 'Grandparent'})
+
+    model = DrillConceptLearner(knowledge_base=kb,
+                                refinement_operator=rho,
+                                quality_func=F1(),
+                                heuristic_func=DrillHeuristic(model=trainer.model),
+                                instance_emb=instance_emb,
+                                search_tree=SearchTreePriorityQueue(),
+                                min_length=1,  # think better variable name
+                                terminate_on_goal=True,
+                                iter_bound=1_000,
+                                max_num_of_concepts_tested=5_000,
+                                ignored_concepts={},
+                                verbose=True)
+    model.predict(pos=p, neg=n)
+    model.best_hypotheses(top_n=10)
+    model.save_best_hypotheses(file_path=str_target_concept + '_best_hypothesis.owl', top_n=10)
