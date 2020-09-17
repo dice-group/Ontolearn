81e6be1cb96ea2d916157ec340df0b6bdf78ebd9
Caglar Demir <caglardemir8@gmail.com>
2020-09-17 07:24:07 +0200

    looping over randomly generated concepts added.

--- examples/custom_learning_problem_generator.py
+++ examples/custom_learning_problem_generator.py
@@ -11,13 +11,5 @@ lp_gen = LearningProblemGenerator(knowledge_base=kb,
                                   refinement_operator=rho,
                                   num_problems=10, depth=2, min_length=4)
 
-random_concepts=[]
-for path in lp_gen:
-    for p in path:
-        print(p)  # node
-        # p.concept =>  concept
-        # p.concept.str => # string representation of concept.
-    random_concepts.append(p) # last item
-
-for i in random_concepts:
-    serialize_concepts(concepts=[i], serialize_name='dummy_'+i.concept.str, metric='F1', attribute='quality', rdf_format='nt')  #
+for concept in lp_gen:
+    print(concept)
