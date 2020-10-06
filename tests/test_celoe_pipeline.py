8509c795adfbe18824d2da9d698c1e6def6aa3af
Stefan Heindorf <heindorf@uni-paderborn.de>
2020-10-06 11:48:34 +0200

    add test

--- tests/test_celoe_pipeline.py
+++ tests/test_celoe_pipeline.py
@@ -66,3 +66,33 @@ def test_celoe():
 
         model.predict(pos=p, neg=n)
         model.show_best_predictions(top_n=10_000, key='heuristic')
+
+
+def test_celoe_predictions():
+    with open('examples/synthetic_problems.json') as json_file:
+        settings = json.load(json_file)
+
+    # because '../data/family-benchmark_rich_background.owl'
+    kb = KnowledgeBase(path=settings['data_path'][3:])
+
+    for str_target_concept, examples in settings['problems'].items():
+        if str_target_concept == 'Aunt':
+            p = set(examples['positive_examples'])
+            n = set(examples['negative_examples'])
+            print('Target concept: ', str_target_concept)
+            model = CELOE(knowledge_base=kb,
+                          refinement_operator=ModifiedCELOERefinement(kb=kb),
+                          quality_func=F1(),
+                          min_horizontal_expansion=0,
+                          heuristic_func=CELOEHeuristic(),
+                          search_tree=CELOESearchTree(),
+                          terminate_on_goal=True,
+                          iter_bound=100,
+                          verbose=False)
+
+            best_pred = model.predict(pos=p, neg=n)
+            model.show_best_predictions(top_n=10, key='quality')
+
+            # TODO: The quality should be at least 0.8 as in the previous
+            #  version (without the world object)
+            assert (best_pred.quality > 0.7)
