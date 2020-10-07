4090d8064652d3a59b72984fb7c365890f0e0f7b
Caglar Demir <caglardemir8@gmail.com>
2020-10-07 17:48:40 +0200

    test are updated so that ontology parsing problem due to World() does not occur.

--- tests/test_refinement_operators.py
+++ tests/test_refinement_operators.py
@@ -1,9 +1,12 @@
 """ Test for refinement_operators.py"""
 from ontolearn import KnowledgeBase, CustomRefinementOperator
+import json
+with open('examples/synthetic_problems.json') as json_file:
+    settings = json.load(json_file)
 
-
+# because '../data/family-benchmark_rich_background.owl'
+kb = KnowledgeBase(path=settings['data_path'][3:])
 def test_refinement_operator():
-    kb = KnowledgeBase(path='data/family-benchmark_rich_background.owl')
     rho = CustomRefinementOperator(kb)
     for _ in enumerate(rho.refine(kb.thing)):
         pass
