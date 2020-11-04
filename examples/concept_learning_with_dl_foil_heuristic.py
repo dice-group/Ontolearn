2dfae8f0e03f37d6184fdb149a2a8720b9a6c472
Caglar Demir <caglardemir8@gmail.com>
2020-11-04 14:06:11 +0100

    Not used imports removed.

--- examples/concept_learning_with_dl_foil_heuristic.py
+++ examples/concept_learning_with_dl_foil_heuristic.py
@@ -1,11 +1,6 @@
 import json
-
 from ontolearn import KnowledgeBase
 from ontolearn.concept_learner import CustomConceptLearner
-from ontolearn.heuristics import DLFOILHeuristic
-from ontolearn.metrics import F1
-from ontolearn.refinement_operators import CustomRefinementOperator
-from ontolearn.search import SearchTree
 
 with open('synthetic_problems.json') as json_file:
     settings = json.load(json_file)
@@ -17,9 +12,6 @@ for str_target_concept, examples in settings['problems'].items():
     n = set(examples['negative_examples'])
     print('Target concept: ', str_target_concept)
     model = CustomConceptLearner(knowledge_base=kb)
-
     model.fit(pos=p, neg=n)
     hypotheses = model.best_hypotheses(n=2)
-    for h in hypotheses:
-        print(h)
-    predictions = model.predict(individuals=list(n), hypotheses=hypotheses)
+    print(hypotheses[0])
--- examples/concept_learning_with_ocel.py
+++ examples/concept_learning_with_ocel.py
@@ -1,17 +1,11 @@
 import json
-
 from ontolearn import KnowledgeBase
 from ontolearn.concept_learner import OCEL
-from ontolearn.heuristics import OCELHeuristic
-from ontolearn.metrics import F1
-from ontolearn.refinement_operators import ModifiedCELOERefinement
-from ontolearn.search import CELOESearchTree
 
 with open('synthetic_problems.json') as json_file:
     settings = json.load(json_file)
 
 kb = KnowledgeBase(path=settings['data_path'])
-
 for str_target_concept, examples in settings['problems'].items():
     p = set(examples['positive_examples'])
     n = set(examples['negative_examples'])
@@ -20,10 +14,12 @@ for str_target_concept, examples in settings['problems'].items():
     # lets inject more background info
     if str_target_concept in ['Granddaughter', 'Aunt', 'Sister']:
         concepts_to_ignore.update(
-            {'http://www.benchmark.org/family#Brother', 'Father', 'Grandparent'}) # Use URI, or concept with length 1.
-    model = OCEL(knowledge_base=kb)
+            {'http://www.benchmark.org/family#Brother',
+             'Father', 'http://www.benchmark.org/family#Grandparent'})  # Use URI, or concept with length 1.
+
+    model = OCEL(knowledge_base=kb,
+                 ignored_concepts=concepts_to_ignore)
 
     model.fit(pos=p, neg=n)
-    hypotheses = model.best_hypotheses(n=10)
-    predictions = model.predict(individuals=list(p), hypotheses=hypotheses)
-    print(predictions)
+    hypotheses = model.best_hypotheses(n=1)
+    print(hypotheses[0])
