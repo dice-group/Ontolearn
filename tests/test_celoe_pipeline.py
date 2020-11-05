20efe3a160d8de4cf0e893cda49fee7205e76015
Stefan Heindorf <heindorf@uni-paderborn.de>
2020-11-05 13:48:53 +0100

    integrate father tests

--- tests/test_celoe_pipeline.py
+++ tests/test_celoe_pipeline.py
@@ -9,6 +9,8 @@ from ontolearn import F1
 from ontolearn import KnowledgeBase
 from ontolearn import ModifiedCELOERefinement
 
+PATH_DATA_FATHER = 'data/father.owl'
+
 with open('examples/synthetic_problems.json') as json_file:
     settings = json.load(json_file)
 # because '../data/family-benchmark_rich_background.owl'
@@ -36,7 +38,6 @@ def test_celoe():
     with open('examples/synthetic_problems.json') as json_file:
         settings = json.load(json_file)
 
-
     for str_target_concept, examples in settings['problems'].items():
         p = set(examples['positive_examples'])
         n = set(examples['negative_examples'])
@@ -58,7 +59,7 @@ def test_celoe():
                       ignored_concepts=concepts_to_ignore,
                       verbose=False)
 
-        #model.fit(pos=p, neg=n)
+        model.fit(pos=p, neg=n)
 
 
 def test_celoe_predictions():
@@ -84,3 +85,43 @@ def test_celoe_predictions():
 
             best_preds = model.best_hypotheses(n=1)
             assert best_preds[0].quality > 0.8
+
+
+def test_celoe_father():
+    kb = KnowledgeBase(path=PATH_DATA_FATHER)
+    # with (kb.onto):
+    #    sync_reasoner()
+    # sync_reasoner()
+
+    examples = {
+        'positive_examples': [
+            "http://example.com/father#stefan",
+            "http://example.com/father#markus",
+            "http://example.com/father#martin"],
+        'negative_examples': [
+            "http://example.com/father#heinz",
+            "http://example.com/father#anna",
+            "http://example.com/father#michelle"]
+    }
+
+    p = set(examples['positive_examples'])
+    n = set(examples['negative_examples'])
+    concepts_to_ignore = set()
+
+    model = CELOE(knowledge_base=kb,
+                  refinement_operator=ModifiedCELOERefinement(kb=kb),
+                  quality_func=F1(),
+                  min_horizontal_expansion=0.02,
+                  heuristic_func=CELOEHeuristic(),
+                  search_tree=CELOESearchTree(),
+                  terminate_on_goal=True,
+                  iter_bound=137141,
+                  max_num_of_concepts_tested=3000,
+                  ignored_concepts=concepts_to_ignore,
+                  verbose=False)
+
+    model.fit(pos=p, neg=n)
+    best_pred = model.best_hypotheses(n=1)[0]
+    print(best_pred)
+    assert(best_pred.quality == 1.0)
+    assert(best_pred.concept.str == '(male  ⊓  (∃hasChild.person))')
