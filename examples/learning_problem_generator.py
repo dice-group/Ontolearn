e22cf6be1083486f7281ae6a0c3873bb80ad4615
Simon Bin <sbin@informatik.uni-leipzig.de>
2021-02-01 18:15:54 +0100

    [base] port the learning_problem_generator

--- examples/learning_problem_generator.py
+++ examples/learning_problem_generator.py
@@ -1,16 +1,20 @@
 #from ontolearn import KnowledgeBase, LearningProblemGenerator
 from ontolearn.base import KnowledgeBase
 from ontolearn.learning_problem_generator import LearningProblemGenerator
+from ontolearn.refinement_operators import ModifiedCELOERefinement
 from ontolearn.static_funcs import export_concepts
 
 path = '../KGs/Biopax/biopax.owl'
 
-kb = KnowledgeBase(path)
+kb = KnowledgeBase(path=path)
 lp = LearningProblemGenerator(knowledge_base=kb)
+num_inds = kb.individuals_count()
 concepts = lp.get_concepts(num_problems=1000,
                            num_diff_runs=10,
-                           min_num_instances=int(len(kb.individuals) * .1),
-                           max_num_instances=int(len(kb.individuals) * .8),
+                           min_num_instances=int(num_inds * .1),
+                           max_num_instances=int(num_inds * .8),
                            min_length=3, max_length=10)
 # Each generated concept defines the type information of min 10% and max 80% of instances.
-export_concepts(kb, list(concepts), path='example_concepts.owl')
+for c in concepts:
+    print('*', c)
+#export_concepts(kb, list(concepts), path='example_concepts.owl')
