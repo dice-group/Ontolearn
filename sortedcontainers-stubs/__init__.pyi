336642259745a368c6ccbecc3c00f0ce565758c6
Simon Bin <sbin@informatik.uni-leipzig.de>
2021-02-05 23:17:22 +0100

    add sortedcontainers pyi files

---
+++ sortedcontainers-stubs/__init__.pyi
@@ -0,0 +1,19 @@
+from .sortedlist import SortedList, SortedKeyList, SortedListWithKey
+from .sortedset import SortedSet
+from .sorteddict import (
+    SortedDict,
+    SortedKeysView,
+    SortedItemsView,
+    SortedValuesView,
+)
+
+__all__ = [
+    'SortedList',
+    'SortedKeyList',
+    'SortedListWithKey',
+    'SortedDict',
+    'SortedKeysView',
+    'SortedItemsView',
+    'SortedValuesView',
+    'SortedSet',
+]
