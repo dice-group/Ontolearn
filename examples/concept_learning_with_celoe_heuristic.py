415f2ae5b5a7410282c7529803f5548cb829e8d3
Simon Bin <sbin@informatik.uni-leipzig.de>
2021-02-16 09:39:33 +0100

    [base] continue updating CELOE example

--- examples/concept_learning_with_celoe_heuristic.py
+++ examples/concept_learning_with_celoe_heuristic.py
@@ -1,6 +1,8 @@
 import json
 from ontolearn import KnowledgeBase
 from ontolearn.concept_learner import CELOE
+from ontolearn.owlapy import IRI
+from ontolearn.owlapy.model import OWLClass, OWLNamedIndividual
 
 with open('synthetic_problems.json') as json_file:
     settings = json.load(json_file)
@@ -16,12 +18,18 @@ for str_target_concept, examples in settings['problems'].items():
     concepts_to_ignore = set()
     # lets inject more background info
     if str_target_concept in ['Granddaughter', 'Aunt', 'Sister']:
+        NS = 'http://www.benchmark.org/family#'
         concepts_to_ignore.update(
-            {'http://www.benchmark.org/family#Brother',
-             'Father', 'http://www.benchmark.org/family#Grandparent'})  # Use URI, or concept with length 1.
-    model.fit(pos=p, neg=n, ignore=concepts_to_ignore)
+            {OWLClass(IRI(NS, 'Brother')),
+             OWLClass(IRI(NS, 'Father')),
+             OWLClass(IRI(NS, 'Grandparent')),
+             })  # Use OWLClass
+    model.fit(pos=set(map(OWLNamedIndividual, map(IRI.create, p))),
+              neg=set(map(OWLNamedIndividual, map(IRI.create, n))),
+              ignore=concepts_to_ignore)
     model.save_best_hypothesis(n=3, path='Predictions_{0}'.format(str_target_concept))
     # Get Top n hypotheses
     hypotheses = model.best_hypotheses(n=3)
     # Use hypotheses as binary function to label individuals.
     predictions = model.predict(individuals=list(p) + list(n), hypotheses=hypotheses)
+    exit(1)
--- examples/learning_problem_generator.py
+++ examples/learning_problem_generator.py
@@ -7,14 +7,15 @@ from ontolearn.static_funcs import export_concepts
 
 path = '../KGs/Biopax/biopax.owl'
 
-kb = KnowledgeBase(path=path, reasoner_factory=OWLReasoner_Owlready2_TempClasses)
+#kb = KnowledgeBase(path=path, reasoner_factory=OWLReasoner_Owlready2_TempClasses)
+kb = KnowledgeBase(path=path)
 lp = LearningProblemGenerator(knowledge_base=kb)
 num_inds = kb.individuals_count()
 concepts = lp.get_concepts(num_problems=1000,
                            num_diff_runs=10,
-                           min_num_instances=int(num_inds * .1),
-                           max_num_instances=int(num_inds * .8),
-                           min_length=3, max_length=10)
+                           min_num_instances=int(1),
+                           max_num_instances=int(num_inds * .9),
+                           min_length=3, max_length=40)
 # Each generated concept defines the type information of min 10% and max 80% of instances.
 for c in concepts:
     print('*', c)
