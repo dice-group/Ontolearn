67a766f8397952517fb0eebdf0b1a9b19f5aed5a
Simon Bin <sbin@informatik.uni-leipzig.de>
2021-02-16 10:18:25 +0100

    [base] update celoe learning example

--- examples/concept_learning_with_celoe_heuristic.py
+++ examples/concept_learning_with_celoe_heuristic.py
@@ -1,14 +1,19 @@
 import json
+import random
+
 from ontolearn import KnowledgeBase
 from ontolearn.concept_learner import CELOE
 from ontolearn.owlapy import IRI
 from ontolearn.owlapy.model import OWLClass, OWLNamedIndividual
+from ontolearn.owlapy.render import DLSyntaxRenderer
+from ontolearn.search import OENode
 
 with open('synthetic_problems.json') as json_file:
     settings = json.load(json_file)
 
 kb = KnowledgeBase(path=settings['data_path'])
-model = CELOE(knowledge_base=kb, max_runtime=1)
+
+random.seed(0)
 
 # noinspection DuplicatedCode
 for str_target_concept, examples in settings['problems'].items():
@@ -21,15 +26,26 @@ for str_target_concept, examples in settings['problems'].items():
         NS = 'http://www.benchmark.org/family#'
         concepts_to_ignore.update(
             {OWLClass(IRI(NS, 'Brother')),
+             OWLClass(IRI(NS, 'Sister')),
+             OWLClass(IRI(NS, 'Daughter')),
+             OWLClass(IRI(NS, 'Mother')),
+             OWLClass(IRI(NS, 'Grandmother')),
              OWLClass(IRI(NS, 'Father')),
              OWLClass(IRI(NS, 'Grandparent')),
              })  # Use OWLClass
-    model.fit(pos=set(map(OWLNamedIndividual, map(IRI.create, p))),
-              neg=set(map(OWLNamedIndividual, map(IRI.create, n))),
+
+    typed_pos = list(map(OWLNamedIndividual, map(IRI.create, p)))
+    typed_neg = list(map(OWLNamedIndividual, map(IRI.create, n)))
+    model = CELOE(knowledge_base=kb, max_runtime=600, verbose=2, max_num_of_concepts_tested=10_000_000_000, iter_bound=10_000_000_000)
+    model.fit(pos=set(typed_pos),
+              neg=set(typed_neg),
               ignore=concepts_to_ignore)
-    model.save_best_hypothesis(n=3, path='Predictions_{0}'.format(str_target_concept))
+
+    # model.save_best_hypothesis(n=3, path='Predictions_{0}'.format(str_target_concept))
     # Get Top n hypotheses
-    hypotheses = model.best_hypotheses(n=3)
+    hypotheses = list(model.best_hypotheses(n=3))
     # Use hypotheses as binary function to label individuals.
-    predictions = model.predict(individuals=list(p) + list(n), hypotheses=hypotheses)
+    predictions = model.predict(individuals=typed_pos + typed_neg, hypotheses=hypotheses)
+    # print(predictions)
+    [print(_) for _ in hypotheses]
     exit(1)
