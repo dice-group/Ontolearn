7922a28443e2abbefca10dfc4b50fad1b894edf5
Simon Bin <sbin@informatik.uni-leipzig.de>
2021-02-16 09:34:20 +0100

    [base] test refinement operator

--- tests/test_refinement_operators.py
+++ tests/test_refinement_operators.py
@@ -1,12 +1,34 @@
 """ Test for refinement_operators.py"""
-from ontolearn import KnowledgeBase, CustomRefinementOperator
 import json
+
+from ontolearn import KnowledgeBase
+from ontolearn.owlapy.render import DLSyntaxRenderer
+from ontolearn.refinement_operators import CustomRefinementOperator, ModifiedCELOERefinement, LengthBasedRefinement
+from ontolearn.search import Node
+
 with open('examples/synthetic_problems.json') as json_file:
     settings = json.load(json_file)
 # because '../data/family-benchmark_rich_background.owl'
 kb = KnowledgeBase(path=settings['data_path'][3:])
 
-def test_refinement_operator():
-    rho = CustomRefinementOperator(kb)
-    for _ in enumerate(rho.refine(kb.thing)):
+
+def test_celoe_refinement_operator():
+    r = DLSyntaxRenderer()
+    rho = ModifiedCELOERefinement(kb)
+    for _ in enumerate(rho.refine(Node(kb.thing, root=True), max_length=10, current_domain=kb.thing)):
+        print(r.render(_[1]))
         pass
+
+
+def test_length_refinement_operator():
+    r = DLSyntaxRenderer()
+    rho = LengthBasedRefinement(kb)
+    for _ in enumerate(rho.refine(Node(kb.thing, root=True), max_length=10, apply_combinations=False)):
+        print(r.render(_[1]))
+        pass
+
+
+if __name__ == '__main__':
+    test_celoe_refinement_operator()
+    test_length_refinement_operator()
+
