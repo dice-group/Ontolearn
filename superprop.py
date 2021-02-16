aa3dc370a1c862b4acc373b511c0b42c0fe3ebf0
Simon Bin <sbin@informatik.uni-leipzig.de>
2021-02-16 09:52:14 +0100

    superprop: remove debugging prints

--- superprop.py
+++ superprop.py
@@ -16,27 +16,21 @@ class SuperProp:
     """
 
     def __init__(self, super_obj):
-        print("made SuperProp for super:", super_obj)
         object.__setattr__(self, 'super_obj', super_obj)
 
     def _find_desc(self, name):
         super_obj = object.__getattribute__(self, 'super_obj')
-        print("searching for", name)
         if name != '__class__':
             mro = iter(super_obj.__thisclass__.__mro__)
             for cls in mro:
-                print("A)checking if ", cls, "==", super_obj.__thisclass__)
                 if cls == super_obj.__thisclass__:
                     break
             for cls in mro:
-                print("B)checking if ", cls, " isinstance ", type)
                 if isinstance(cls, type):
                     try:
                         # don't lookup further up the chain
-                        print("calling __getattribute__ on ", cls)
                         return object.__getattribute__(cls, name)
                     except AttributeError as e:
-                        print("failed with AttributeError", e)
                         continue
                     except KeyError:
                         return None
@@ -46,12 +40,8 @@ class SuperProp:
         return getattr(object.__getattribute__(self, 'super_obj'), name)
 
     def __setattr__(self, name, value):
-        print("invoking __setattr__ for ", name, " to ", value)
         super_obj = object.__getattribute__(self, 'super_obj')
-        print("the super_obj is ", super_obj)
-        print("trying to _find_desc ", name)
         desc = object.__getattribute__(self, '_find_desc')(name)
-        print("desc found: ", desc)
         if hasattr(desc, '__set__'):
             return desc.__set__(super_obj.__self__, value)
         return setattr(super_obj, name, value)
