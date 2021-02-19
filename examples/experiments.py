c78e5739efaff206ae9553434dbe5153defd9a94
Caglar Demir <caglardemir8@gmail.com>
2021-02-19 12:47:52 +0100

    Experiment script is ready to be used for state-of-the-art models.

--- examples/experiments.py
+++ examples/experiments.py
@@ -8,23 +8,28 @@ import os
 
 
 def sanity_checking_args(args):
-    assert os.path.isfile(args.path_knowledge_base)
+    try:
+        assert os.path.isfile(args.path_knowledge_base)
+    except AssertionError as e:
+        print(f'--path_knowledge_base ***{args.path_knowledge_base}*** does not lead to a file.')
+        exit(1)
+
     assert os.path.isfile(args.path_knowledge_base_embeddings)
     assert args.min_num_concepts > 0
-    assert args.min_length_of_concepts > 0
+    assert args.min_length > 0
     assert os.path.isfile(args.path_knowledge_base)
 
 
 def start(args):
     sanity_checking_args(args)
     kb = KnowledgeBase(args.path_knowledge_base)
-    emb = pd.read_csv(args.path_knowledge_base_embeddings, index_col=0)
 
     lp = LearningProblemGenerator(knowledge_base=kb)
-    balanced_examples = lp.get_balanced_examples(min_num_problems=args.min_num_concepts,
-                                                 num_diff_runs=1,  # This must be optimized
-                                                 min_length=args.min_length_of_concepts,
-                                                 min_num_instances=args.min_num_instances_per_concept)
+    balanced_examples = lp.get_balanced_n_samples_per_examples(n=args.num_of_randomly_created_problems_per_concept,
+                                                               min_num_problems=args.min_num_concepts,
+                                                               num_diff_runs=1,  # This must be optimized
+                                                               min_length=args.min_length,
+                                                               min_num_instances=args.min_num_instances_per_concept)
 
     # Initialize models
     celoe = DLLearnerBinder(binary_path=args.path_dl_learner, kb_path=args.path_knowledge_base, model='celoe')
@@ -35,40 +40,51 @@ def start(args):
     ocel_python = OCEL(knowledge_base=kb, verbose=0)
     dl_foil = CustomConceptLearner(knowledge_base=kb, heuristic_func=DLFOILHeuristic(), verbose=0)
 
-    drill_average = DrillAverage(knowledge_base=kb, instance_embeddings=emb, num_episode=1, verbose=0)
-    drill_sample = DrillSample(knowledge_base=kb, instance_embeddings=emb, num_episode=1, verbose=0)
+    drill_average = DrillAverage(knowledge_base=kb,
+                                 path_of_embeddings=args.path_knowledge_base_embeddings,
+                                 num_episode=1, verbose=0,
+                                 num_workers=args.num_workers)
+    drill_sample = DrillSample(knowledge_base=kb,
+                               path_of_embeddings=args.path_knowledge_base_embeddings,
+                               num_episode=1, verbose=0,
+                               num_workers=args.num_workers)
 
-    exp = Experiments()
-    k_fold_cross_validation = exp.start_KFold(k=args.num_fold_for_k_fold_cv, dataset=balanced_examples,
-                                              models=[drill_average, drill_sample, celoe_python, ocel_python, dl_foil,
-                                                      celoe, ocel, eltl],
-                                              max_runtime_per_problem=args.max_num_seconds_for_search_per_concept)
-    print('\n##### K-FOLD CROSS VALUATION RESULTS #####')
-    for k, v in k_fold_cross_validation.items():
-        f1 = v['F-measure']
-        acc = v['Accuracy']
-        runtime = v['Runtime']
-        fol_result = '{}\t F-measure:(avg.{:.2f} | std.{:.2f})\tAccuracy:(avg.{:.2f} | std.{:.2f})\t' \
-                     'Runtime:(avg.{:.2f} | std.{:.2f})'.format(k,
-                                                                f1.mean(), f1.std(),
-                                                                acc.mean(),
-                                                                acc.std(),
-                                                                runtime.mean(), runtime.std())
-        print(fol_result)
+    Experiments(max_test_time_per_concept=args.max_test_time_per_concept).start_KFold(k=args.num_fold_for_k_fold_cv,
+                                                                                      dataset=balanced_examples,
+                                                                                      models=[
+                                                                                          # drill_average,drill_sample,
+                                                                                          # celoe_python, ocel_python,
+                                                                                          # dl_foil,
+                                                                                          celoe, ocel, eltl])
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
+    # General
     parser.add_argument("--path_knowledge_base", type=str,
-                        default='/home/demir/Desktop/Onto-learn_dev/data/family-benchmark_rich_background.owl')
-    parser.add_argument("--path_knowledge_base_embeddings", type=str,
-                        default='../embeddings/dismult_family_benchmark/instance_emb.csv')
+                        default='/home/demir/Desktop/Onto-learn_dev/KGs/Family/family-benchmark_rich_background.owl')
+    parser.add_argument("--verbose", type=int, default=0)
+    parser.add_argument('--num_workers', type=int, default=32, help='Number of cpus used during batching')
     parser.add_argument("--path_dl_learner", type=str, default='/home/demir/Desktop/DL/dllearner-1.4.0/')
 
+    # Concept Generation Related
     parser.add_argument("--min_num_concepts", type=int, default=10)
-    parser.add_argument("--min_length_of_concepts", type=int, default=2)
-    parser.add_argument("--min_num_instances_per_concept", type=int, default=5)
-    parser.add_argument("--max_num_seconds_for_search_per_concept", type=int, default=5)
-    parser.add_argument("--num_fold_for_k_fold_cv", type=int, default=2)
+    parser.add_argument("--min_length", type=int, default=5, help='Min length of concepts to be used')
+    parser.add_argument("--max_length", type=int, default=6, help='Max length of concepts to be used')
+    parser.add_argument("--min_num_instances_per_concept", type=int, default=10)
+    parser.add_argument("--num_of_randomly_created_problems_per_concept", type=int, default=5)
 
+    # Evaluation related
+    parser.add_argument('--num_fold_for_k_fold_cv', type=int, default=10, help='Number of cpus used during batching')
+    parser.add_argument('--max_test_time_per_concept', type=int, default=5,
+                        help='Maximum allowed runtime during testing')
+
+    # DQL related
+    parser.add_argument("--path_knowledge_base_embeddings", type=str,
+                        default='../embeddings/Shallom_Family/Shallom_entity_embeddings.csv')
+    parser.add_argument("--num_episode", type=int, default=2)
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument('--num_of_sequential_actions', type=int, default=2)
+    parser.add_argument('--pretrained_drill_sample_path', type=str, default='', help='Provide a path of .pth file')
+    parser.add_argument('--pretrained_drill_avg_path', type=str, default='', help='Provide a path of .pth file')
     start(parser.parse_args())
