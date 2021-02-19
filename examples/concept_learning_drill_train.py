70f979d7ce48d5f6cfccf0e1663fe332fe6c710a
Caglar Demir <caglardemir8@gmail.com>
2021-02-19 12:49:23 +0100

    Comments for parameters for Drills scripts are added.

--- examples/concept_learning_drill_train.py
+++ examples/concept_learning_drill_train.py
@@ -39,19 +39,30 @@ def start(args):
 
 if __name__ == '__main__':
     parser = ArgumentParser()
+    # General
     parser.add_argument("--path_knowledge_base", type=str,
                         default='/home/demir/Desktop/Onto-learn_dev/KGs/Family/family-benchmark_rich_background.owl')
-    parser.add_argument("--path_knowledge_base_embeddings", type=str,
-                        default='../embeddings/Shallom_Family/Shallom_entity_embeddings.csv')
-    parser.add_argument("--min_num_concepts", type=int, default=2)
-    parser.add_argument("--min_length", type=int, default=3, help='Min length of concepts to be used')
-    parser.add_argument("--max_length", type=int, default=5, help='Max length of concepts to be used')
-    parser.add_argument("--min_num_instances_per_concept", type=int, default=1)
-    parser.add_argument("--num_of_randomly_created_problems_per_concept", type=int, default=2)
-    parser.add_argument("--num_episode", type=int, default=2)
-    parser.add_argument("--batch_size", type=int, default=32)
     parser.add_argument("--verbose", type=int, default=0)
     parser.add_argument('--num_workers', type=int, default=32, help='Number of cpus used during batching')
+
+    # Concept Generation Related
+    parser.add_argument("--min_num_concepts", type=int, default=2)
+    parser.add_argument("--min_length", type=int, default=3, help='Min length of concepts to be used')
+    parser.add_argument("--max_length", type=int, default=6, help='Max length of concepts to be used')
+    parser.add_argument("--min_num_instances_per_concept", type=int, default=1)
+    parser.add_argument("--num_of_randomly_created_problems_per_concept", type=int, default=2)
+
+    # Evaluation related
+    parser.add_argument('--num_fold_for_k_fold_cv', type=int, default=3, help='Number of cpus used during batching')
+    parser.add_argument('--max_test_time_per_concept', type=int, default=3,
+                        help='Maximum allowed runtime during testing')
+    # DQL related
+    parser.add_argument("--path_knowledge_base_embeddings", type=str,
+                        default='../embeddings/Shallom_Family/Shallom_entity_embeddings.csv')
+    parser.add_argument("--num_episode", type=int, default=2)
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument('--num_of_sequential_actions', type=int, default=2)
     parser.add_argument('--pretrained_drill_sample_path', type=str, default='', help='Provide a path of .pth file')
     parser.add_argument('--pretrained_drill_avg_path', type=str, default='', help='Provide a path of .pth file')
+
     start(parser.parse_args())
