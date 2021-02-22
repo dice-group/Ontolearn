254bc8c4ad02e0f86f59108c5a6828b898579254
Simon Bin <sbin@informatik.uni-leipzig.de>
2021-02-22 23:04:23 +0100

    port the LearningProblemGenerator to OWLAPy again
    Todo: exporting of concepts

--- examples/learning_problem_generator.py
+++ examples/learning_problem_generator.py
@@ -1,16 +1,13 @@
 #from ontolearn import KnowledgeBase, LearningProblemGenerator
 from ontolearn.knowledge_base import KnowledgeBase
 from ontolearn.learning_problem_generator import LearningProblemGenerator
-from owlapy.owlready2.temp_classes import OWLReasoner_Owlready2_TempClasses
-from ontolearn.refinement_operators import ModifiedCELOERefinement
-from ontolearn.static_funcs import export_concepts
 from ontolearn.utils import setup_logging
 
 setup_logging("logging_test.conf")
 
 path = '../KGs/Biopax/biopax.owl'
 
-#kb = KnowledgeBase(path=path, reasoner_factory=OWLReasoner_Owlready2_TempClasses)
+# kb = KnowledgeBase(path=path, reasoner_factory=OWLReasoner_Owlready2_TempClasses)
 kb = KnowledgeBase(path=path)
 lp = LearningProblemGenerator(knowledge_base=kb)
 num_inds = kb.individuals_count()
@@ -22,4 +19,6 @@ concepts = lp.get_concepts(num_problems=1000,
 # Each generated concept defines the type information of min 10% and max 80% of instances.
 for c in concepts:
     print('*', c)
-#export_concepts(kb, list(concepts), path='example_concepts.owl')
+
+# TODO
+# export_concepts(kb, list(concepts), path='example_concepts.owl')
