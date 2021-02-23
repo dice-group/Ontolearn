7d9ee95c3cabdb9d3cb17901880bb00cd3dabeca
Simon Bin <sbin@informatik.uni-leipzig.de>
2021-02-23 09:38:59 +0100

    delete leftover code from search

--- ontolearn/search.py
+++ ontolearn/search.py
@@ -2,112 +2,17 @@ import weakref
 from _weakref import ReferenceType
 from functools import total_ordering
 from queue import PriorityQueue
-from typing import List, Optional, ClassVar, Callable, cast, Final, Iterable, TypeVar, Generic, Set
+from typing import List, Optional, ClassVar, Final, Iterable, TypeVar, Generic, Set
 
-from sortedcontainers import SortedSet
-
-from superprop import super_prop
-from .abstracts import AbstractTree, AbstractScorer, AbstractNode
-from .core.owl.utils import OrderedOWLObject
 from owlapy.io import OWLObjectRenderer
 from owlapy.model import OWLClassExpression
 from owlapy.render import DLSyntaxRenderer
 from owlapy.utils import as_index
+from .abstracts import AbstractTree, AbstractNode
+from .core.owl.utils import OrderedOWLObject
 
 _N = TypeVar('_N')
 
-
-# class Node(AbstractNode):
-#     __slots__ = 'individuals_count'
-#
-#     renderer: ClassVar[OWLObjectRenderer] = DLSyntaxRenderer()
-#     individuals_count: Optional[int]
-#
-#     def __init__(self: _N, concept: OWLClassExpression, parent_node: Optional[_N] = None, is_root: bool = False):
-#         # we have to initialise our attributes before calling super, since the parent __init__ will add ourselves to
-#         # the parent node
-#         self.individuals_count = None
-#         super().__init__(concept, parent_node=parent_node, is_root=is_root)
-#
-#     def __eq__(self, other):
-#         return self.concept == other.concept and self.parent_node is other.parent_node
-#
-#     def __hash__(self):
-#         return hash(self.concept)
-#
-#     def __str__(self):
-#         return f'Node at {hex(id(self))}\t{Node.renderer.render(self.concept)}\tQuality:{self.quality}\t' \
-#                f'Heuristic:{self.heuristic}\tDepth:{self.depth}\t' \
-#                f'|Children|:{self.refinement_count}\t|Indv.|:{self.individuals_count}'
-#
-#
-# class OENode(Node):
-#     __slots__ = '_horizontal_expansion', '_len'
-#
-#     _horizontal_expansion: int
-#     _len: Final[int]
-#
-#     def __init__(self, concept: OWLClassExpression, length: int,
-#                  parent_node: Optional['OENode'] = None, is_root: bool = False):
-#         # we have to initialise our attributes before calling super, since the parent __init__ will add ourselves to
-#         # the parent node
-#         self._len = length
-#         self._horizontal_expansion = length
-#         super().__init__(concept, parent_node=parent_node, is_root=is_root)
-#
-#     def __eq__(self, other):
-#         return self.concept == other.concept and self._len == other._len and self.parent_node is other.parent_node
-#
-#     def __hash__(self):
-#         return super().__hash__()
-#
-#     @property
-#     def len(self) -> int:
-#         return self._len
-#
-#     @property
-#     def h_exp(self) -> int:
-#         return self._horizontal_expansion
-#
-#     def increment_h_exp(self, *, extra_inc: int = 0) -> None:
-#         self.heuristic = None
-#         self._horizontal_expansion += extra_inc + 1
-#
-#     @property
-#     def parent_node(self) -> Optional['OENode']:
-#         return super().parent_node
-#
-#     @parent_node.setter
-#     def parent_node(self, n: 'OENode'):
-#         if self.parent_node is not None:
-#             raise ValueError("Node already has parent")
-#         assert isinstance(n, OENode) or n is None
-#         super_prop(super()).parent_node = n
-#
-#     @property
-#     def quality(self) -> float:
-#         return super().quality
-#
-#     @quality.setter
-#     def quality(self, q: float):
-#         self.heuristic = None
-#         if self.quality is not None:
-#             raise ValueError("Node already evaluated")
-#         super_prop(super()).quality = q
-#
-#     @property
-#     def refinement_count(self) -> int:
-#         return super().refinement_count
-#
-#     @refinement_count.setter
-#     def refinement_count(self, c: int):
-#         self.heuristic = None
-#         super_prop(super()).refinement_count = c
-#
-#     def __str__(self):
-#         return f'OENode at {hex(id(self))}\t{Node.renderer.render(self.concept)}\tQuality:{self.quality}\t' \
-#                f'Heuristic:{self.heuristic}\tDepth:{self.depth}\tH_exp:{self.h_exp}\t' \
-#                f'|Children|:{self.refinement_count}\t|Indv.|:{self.individuals_count}'
 
 class OENode(AbstractNode):
     __slots__ = '_concept', '_parent_ref', '_quality', '_heuristic', '_horizontal_expansion', '_len', \
@@ -115,7 +20,7 @@ class OENode(AbstractNode):
 
     renderer: ClassVar[OWLObjectRenderer] = DLSyntaxRenderer()
 
-    _parent_ref: Optional[ReferenceType]  # OENode
+    _parent_ref: Optional[ReferenceType]  # Optional[ReferenceType[OENode]]
     _quality: Optional[float]
     _heuristic: Optional[float]
     _horizontal_expansion: int
@@ -279,117 +184,6 @@ def _node_and_all_children(n: _N) -> Iterable[_N]:
     yield n
     for c in n.children:
         yield from _node_and_all_children(c)
-
-
-# class CELOESearchTree(AbstractTree[OENode]):
-#     __slots__ = 'nodes', 'expression_tests', '_root_ref'
-#
-#     nodes: 'SortedSet[OENode]'
-#     expression_tests: int
-#     _root_ref: Optional[ReferenceType]  # [OENode]
-#
-#     def __init__(self):
-#         super().__init__()
-#         self.clean()
-#
-#     def clean(self):
-#         self.nodes = SortedSet(key=HeuristicOrderedNode)
-#         self._root_ref = None
-#         self.expression_tests = 0
-#
-#     def __len__(self) -> int:
-#         return len(self.nodes)
-#
-#     # def update_prepare(self, n: Node) -> None:
-#     #     """Remove n and its children from search tree.
-#     #
-#     #     Args:
-#     #         n: is a node object containing a concept.
-#     #     """
-#     #     subtree = list(_node_and_all_children(n))
-#     #     self.nodes.difference_update(subtree)
-#
-#     # def update_done(self, n: Node) -> None:
-#     #     """Add back n and its children into search tree.
-#     #
-#     #     Args:
-#     #         n: is a node object containing a concept.
-#     #     """
-#     #     subtree = list(_node_and_all_children(n))
-#     #     self.nodes.update(subtree)
-#
-#     def update_prepare(self, n: OENode) -> None:
-#         """Remove n search tree.
-#
-#         Args:
-#             n: is a node object containing a concept.
-#         """
-#         self.nodes.remove(n)
-#
-#     def update_done(self, n: OENode) -> None:
-#         """Add back n into search tree.
-#
-#         Args:
-#             n: is a node object containing a concept.
-#         """
-#         self.nodes.add(n)
-#
-#     @property
-#     def root(self) -> Optional[OENode]:
-#         return self._root_ref() if self._root_ref is not None else None
-#
-#     # noinspection DuplicatedCode
-#     def add(self, node: OENode):
-#         assert isinstance(node, OENode)
-#         if node not in self.nodes:
-#             if node.parent_node is not None:
-#                 assert node.parent_node in self.nodes
-#             else:
-#                 assert node.is_root
-#                 assert not self.nodes
-#
-#             if node.is_root:
-#                 self._root_ref = weakref.ref(node)
-#
-#             self.nodes.add(node)
-#
-#             if node.quality == 1:  # goal found
-#                 return True
-#             return False
-#         else:
-#             print("node: ", node)
-#             print("parent node: ", node.parent_node)
-#             for n in self.nodes.irange(minimum=node, maximum=node):
-#                 print("existing node: ", n)
-#                 print("e.p. node: ", n.parent_node)
-#             raise ValueError("Node already exists", node)
-#             # if not (node.parent_node is parent_node):
-#             #     try:
-#             #         assert parent_node.heuristic is not None
-#             #         assert node.parent_node.heuristic is not None
-#             #     except AssertionError:
-#             #         print('REFINED NODE:', parent_node)
-#             #         print('NODE TO BE ADDED:', node)
-#             #         print('previous parent of node to be added', node.parent_node)
-#             #         for k, v in self.nodes.items():
-#             #             print(k)
-#             #         raise ValueError()
-#             #
-#             #     if parent_node.heuristic > node.parent_node.heuristic:
-#             #         """Ignore previous parent"""
-#             #     else:
-#             #         if node in node.parent_node.children:
-#             #             node.parent_node.remove_child(node)
-#             #         node.parent_node = parent_node
-#             #         self.heuristic_func.apply(node, parent_node=parent_node)
-#             #         self.nodes[node] = node
-#
-#     def best_heuristic_node(self) -> OENode:
-#         return self.nodes[-1]
-#
-#     def best_heuristic_nodes(self, n: int = 10) -> Iterable[OENode]:
-#         for i in range(n):
-#             yield self.nodes[-n - 1]
 
 
 class Node(AbstractNode):
