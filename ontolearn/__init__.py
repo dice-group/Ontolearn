ee5428b91624f960efeecd1313695034fe5e7a78
Simon Bin <sbin@informatik.uni-leipzig.de>
2021-03-25 17:54:52 +0100

    fix version

--- ontolearn/__init__.py
+++ ontolearn/__init__.py
@@ -7,7 +7,7 @@ The goal of ontolearn os to provide efficient solutions for concept learning on 
 Author:
     Caglar Demir <caglar.demir@upb.de>,<caglardemir8@gmail.com>
 """
-__version__ = '0.1.4.dev'
+__version__ = '0.2.0.dev'
 
 from .knowledge_base import KnowledgeBase
 
