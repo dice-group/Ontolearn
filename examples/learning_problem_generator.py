20310775cb80386a6571abad07bdc7a891da3460
Simon Bin <sbin@informatik.uni-leipzig.de>
2021-05-31 19:51:14 +0200

    export concepts from learning problem generator

--- examples/learning_problem_generator.py
+++ examples/learning_problem_generator.py
@@ -17,14 +17,13 @@ path = '../KGs/Biopax/biopax.owl'
 kb = KnowledgeBase(path=path)
 lp = LearningProblemGenerator(knowledge_base=kb)
 num_inds = kb.individuals_count()
-concepts = lp.get_concepts(num_problems=1000,
-                           num_diff_runs=10,
-                           min_num_instances=int(1),
-                           max_num_instances=int(num_inds * .9),
-                           min_length=3, max_length=40)
+concepts = list(lp.get_concepts(num_problems=5000,
+                                num_diff_runs=10,
+                                min_num_instances=int(2),
+                                max_num_instances=int(num_inds * .95),
+                                min_length=4, max_length=40))
 # Each generated concept defines the type information of min 10% and max 80% of instances.
-for c in concepts:
-    print('*', c)
+# for c in concepts:
+#     print('*', c)
 
-# TODO
-# export_concepts(kb, list(concepts), path='example_concepts.owl')
+lp.export_concepts(concepts, path='example_concepts')
