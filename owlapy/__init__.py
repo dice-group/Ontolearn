816937afc2c3d5ed9fbe152b9eb48ddcb3596536
Simon Bin <sbin@informatik.uni-leipzig.de>
2021-07-01 21:05:34 +0200

    document the model more thoroughly

--- owlapy/__init__.py
+++ owlapy/__init__.py
@@ -1,3 +1,9 @@
 """OWLAPY
 
-loosely based on OWLAPI"""
+loosely based on OWL API
+
+many help texts copied from OWL API [1]
+OWLAPI licence: LGPL and Apache
+
+[1] https://github.com/owlcs/owlapi
+"""
