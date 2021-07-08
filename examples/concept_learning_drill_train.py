be35ccf9fa11b40423231698660fd9b12f9058e7
Simon Bin <sbin@informatik.uni-leipzig.de>
2021-07-08 18:45:38 +0200

    use closed world reasoner for negation

--- examples/concept_learning_drill_train.py
+++ examples/concept_learning_drill_train.py
@@ -14,7 +14,20 @@ from ontolearn.concept_learner import Drill
 from ontolearn.metrics import F1
 
 def start(args):
-    kb = KnowledgeBase(path=args.path_knowledge_base)
+    from owlapy.model import OWLOntology, OWLReasoner
+    def ClosedWorld_ReasonerFactory(onto: OWLOntology) -> OWLReasoner:
+        from owlapy.owlready2 import OWLOntology_Owlready2
+        from owlapy.owlready2.temp_classes import OWLReasoner_Owlready2_TempClasses
+        from owlapy.fast_instance_checker import OWLReasoner_FastInstanceChecker
+
+        assert isinstance(onto, OWLOntology_Owlready2)
+        base_reasoner = OWLReasoner_Owlready2_TempClasses(ontology=onto)
+        reasoner = OWLReasoner_FastInstanceChecker(ontology=onto,
+                                                   base_reasoner=base_reasoner,
+                                                   negation_default=True)
+        return reasoner
+
+    kb = KnowledgeBase(path=args.path_knowledge_base, reasoner_factory=ClosedWorld_ReasonerFactory)
 
     lp = LearningProblemGenerator(knowledge_base=kb, min_length=args.min_length, max_length=args.max_length)
 
