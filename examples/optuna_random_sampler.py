8135fc758888af16e14235ab8b53e2d27516cd63
Sheetal <sheetal@mail.uni-paderborn.de>
2022-10-29 22:50:17 +0200

    Added 2 more categorical hyperparameters and added function to transform this hyperparameters

--- examples/optuna_random_sampler.py
+++ examples/optuna_random_sampler.py
@@ -5,16 +5,12 @@ from random import shuffle
 from optuna.samplers import RandomSampler
 from optuna.samplers import TPESampler
 import pandas as pd
-from torch import maximum
 import time
 
 from ontolearn.knowledge_base import KnowledgeBase
-from ontolearn.concept_learner import EvoLearner
 from ontolearn.learning_problem import PosNegLPStandard
-from owlapy.model import OWLClass, OWLNamedIndividual, IRI
+from owlapy.model import OWLNamedIndividual, IRI
 from ontolearn.utils import setup_logging
-from ontolearn.metrics import F1
-from owlapy.render import DLSyntaxObjectRenderer
 from wrapper_evolearner import EvoLearnerWrapper
 
 try:
@@ -25,8 +21,10 @@ except FileNotFoundError:
 with open('carcinogenesis_lp.json') as json_file:
     settings = json.load(json_file)
 
-kb = KnowledgeBase(path=settings['data_path'])    
-df = pd.DataFrame(columns=['LP', 'max_runtime', 'tournament_size', 'height_limit','card_limit','value_splitter', 'quality_score'])
+kb = KnowledgeBase(path=settings['data_path'])
+df = pd.DataFrame(columns=['LP', 'max_runtime', 'tournament_size', 'height_limit', 'use_data_properties',
+                           'quality_func', 'card_limit', 'value_splitter', 'quality_score'])
+
 
 class OptunaSamplers():
     def __init__(self, lp, concept):
@@ -42,29 +40,39 @@ class OptunaSamplers():
         self.study_qmc_sampler = optuna.create_study(sampler=self.qmc_sampler)
     
     def write_to_df(self, **space):
-        df.loc[len(df.index)] = [self.concept, space['max_runtime'], space['tournament_size'], space['height_limit'], space['card_limit'],
-        space['value_splitter'], space['quality_score']]
+        df.loc[len(df.index)] = [self.concept, space['max_runtime'], space['tournament_size'], 
+                                 space['height_limit'], space['card_limit'], space['use_data_properties'], 
+                                 space['quality_func'], space['value_splitter'], space['quality_score']]
     
     def convert_to_csv(self, df):
         timestr = time.strftime("%Y%m%d-%H%M%S")
         filename = "Output"+str(timestr)  
-        df.to_csv(filename+".csv",index=False)
+        df.to_csv(filename+".csv", index=False)
 
-    def objective(self,trial):             
+    def objective(self, trial):             
         max_runtime = trial.suggest_int("max_runtime", 10, 20)
         tournament_size = trial.suggest_int("tournament_size", 2, 10)
-        height_limit = trial.suggest_int('height_limit',3, 25)
+        height_limit = trial.suggest_int('height_limit', 3, 25)
         card_limit = trial.suggest_int('card_limit', 5, 10)
-        value_splitter = trial.suggest_categorical('value_splitter',['binning_value_splitter','entropy_value_splitter'])
-        #create a dictionary
+        use_data_properties = trial.suggest_categorical('use_data_properties', ['True', 'False'])
+        quality_func = trial.suggest_categorical('quality_func', ['F1', 'Accuracy'])
+        value_splitter = trial.suggest_categorical('value_splitter', 
+                                                   ['binning_value_splitter', 'entropy_value_splitter'])
+        # create a dictionary
         space = dict()
         space['max_runtime'] = max_runtime
         space['tournament_size'] = tournament_size
         space['height_limit'] = height_limit
         space['card_limit'] = card_limit
+        space['use_data_properties'] = use_data_properties
         space['value_splitter'] = value_splitter
-        #call the wrapper class
-        wrap_obj = EvoLearnerWrapper(knowledge_base=kb, max_runtime=max_runtime, tournament_size=tournament_size, height_limit=height_limit,card_limit=card_limit,value_splitter=value_splitter )
+        space['quality_func'] = quality_func
+        # call the wrapper class
+        wrap_obj = EvoLearnerWrapper(knowledge_base=kb, max_runtime=max_runtime, tournament_size=tournament_size,
+                                     height_limit=height_limit, card_limit=card_limit, 
+                                     use_data_properties=use_data_properties,
+                                     quality_func=quality_func,
+                                     value_splitter=value_splitter)
         model = wrap_obj.get_evolearner_model()
         model.fit(self.lp, verbose=False)
         model.save_best_hypothesis(n=3, path='Predictions_{0}'.format(str_target_concept))
@@ -78,10 +86,10 @@ class OptunaSamplers():
         self.study_random_sampler.optimize(self.objective, n_trials=n_trials)
         print("-------BEST TRIAL-----------")
         print(self.study_random_sampler.best_trial)
-        #best parameter combination
+        # best parameter combination
         print("-------BEST PARAMS----------")
         print(self.study_random_sampler.best_params)
-        #score achieved with best parameter combination
+        # score achieved with best parameter combination
         print("-------BEST Value----------")
         print(self.study_random_sampler.best_value)
 
@@ -89,10 +97,10 @@ class OptunaSamplers():
         self.study_tpe_sampler.optimize(self.objective, n_trials=n_trials)
         print("-------BEST TRIAL-----------")
         print(self.study_tpe_sampler.best_trial)
-        #best parameter combination
+        # best parameter combination
         print("-------BEST PARAMS----------")
         print(self.study_tpe_sampler.best_params)
-        #score achieved with best parameter combination
+        # score achieved with best parameter combination
         print("-------BEST Value----------")
         print(self.study_tpe_sampler.best_value)
 
@@ -100,10 +108,10 @@ class OptunaSamplers():
         self.study_cmaes_sampler.optimize(self.objective, n_trials=n_trials)
         print("-------BEST TRIAL-----------")
         print(self.study_cmaes_sampler.best_trial)
-        #best parameter combination
+        # best parameter combination
         print("-------BEST PARAMS----------")
         print(self.study_cmaes_sampler.best_params)
-        #score achieved with best parameter combination
+        # score achieved with best parameter combination
         print("-------BEST Value----------")
         print(self.study_cmaes_sampler.best_value)
 
@@ -111,21 +119,21 @@ class OptunaSamplers():
         self.study_nsgaii_sampler.optimize(self.objective, n_trials=population_size)
         print("-------BEST TRIAL-----------")
         print(self.study_nsgaii_sampler.best_trial)
-        #best parameter combination
+        # best parameter combination
         print("-------BEST PARAMS----------")
         print(self.study_nsgaii_sampler.best_params)
-        #score achieved with best parameter combination
+        # score achieved with best parameter combination
         print("-------BEST Value----------")
         print(self.study_nsgaii_sampler.best_value)
-    
+
     def get_best_optimization_result_for_qmc_sampler(self, n_trials):
         self.study_qmc_sampler.optimize(self.objective, n_trials=n_trials)
         print("-------BEST TRIAL-----------")
         print(self.study_qmc_sampler.best_trial)
-        #best parameter combination
+        # best parameter combination
         print("-------BEST PARAMS----------")
         print(self.study_qmc_sampler.best_params)
-        #score achieved with best parameter combination
+        # score achieved with best parameter combination
         print("-------BEST Value----------")
         print(self.study_qmc_sampler.best_value)
 
@@ -138,18 +146,18 @@ if __name__ == "__main__":
         typed_pos = list(set(map(OWLNamedIndividual, map(IRI.create, p))))
         typed_neg = list(set(map(OWLNamedIndividual, map(IRI.create, n))))
         
-        #shuffle the Positive and Negative Sample
+        # shuffle the Positive and Negative Sample
         shuffle(typed_pos)
         shuffle(typed_neg)
 
-        #Split the data into Training Set and Test Set
+        # Split the data into Training Set and Test Set
         train_pos = set(typed_pos[:int(len(typed_pos)*0.8)])
         train_neg = set(typed_neg[:int(len(typed_neg)*0.8)])
         test_pos = set(typed_pos[-int(len(typed_pos)*0.2):])
         test_neg = set(typed_neg[-int(len(typed_neg)*0.2):])
         lp = PosNegLPStandard(pos=train_pos, neg=train_neg)
 
-        #create class object and get the optimised result
+        # create class object and get the optimised result
         optuna1 = OptunaSamplers(lp, str_target_concept)
         optuna1.get_best_optimization_result_for_tpe_sampler(10)
         optuna1.convert_to_csv(df)
--- examples/wrapper_evolearner.py
+++ examples/wrapper_evolearner.py
@@ -1,28 +1,43 @@
 from ontolearn.concept_learner import EvoLearner
 from typing import Optional
-from ontolearn.abstracts  import AbstractDrill, AbstractFitness, AbstractScorer, AbstractNode, BaseRefinement, \
-    AbstractHeuristic, EncodedPosNegLPStandardKind
+from ontolearn.abstracts import AbstractFitness, AbstractScorer
 from ontolearn.knowledge_base import KnowledgeBase
 from ontolearn.concept_learner import EvoLearner
-from ontolearn.search import EvoLearnerNode
-from ontolearn.value_splitter import AbstractValueSplitter, BinningValueSplitter, EntropyValueSplitter
+from ontolearn.value_splitter import BinningValueSplitter, EntropyValueSplitter
 from ontolearn.ea_initialization import AbstractEAInitialization, EARandomInitialization, EARandomWalkInitialization
 from ontolearn.ea_algorithms import AbstractEvolutionaryAlgorithm, EASimple
+from ontolearn.metrics import Accuracy, F1
+
 
 class EvoLearnerWrapper:
-    def __init__(self, knowledge_base: KnowledgeBase, quality_func: Optional[AbstractScorer] = None, fitness_func: Optional[AbstractFitness] = None, init_method: Optional[AbstractEAInitialization] = None, algorithm: Optional[AbstractEvolutionaryAlgorithm] = None, mut_uniform_gen: Optional[AbstractEAInitialization] = None, value_splitter: Optional[str] = None, terminate_on_goal: Optional[bool] = None, max_runtime: Optional[int] = None, use_data_properties: bool = True, use_card_restrictions: bool = True, use_inverse: bool = False, tournament_size: int = 7, card_limit: int = 10, population_size: int = 800, num_generations: int = 200, height_limit: int = 17):
-        self.EvoLearner = EvoLearner
+    def __init__(self, knowledge_base: KnowledgeBase, quality_func: Optional[AbstractScorer] = None, 
+                 fitness_func: Optional[AbstractFitness] = None, 
+                 init_method: Optional[AbstractEAInitialization] = None,
+                 algorithm: Optional[AbstractEvolutionaryAlgorithm] = None,
+                 mut_uniform_gen: Optional[AbstractEAInitialization] = None,
+                 value_splitter: Optional[str] = None, 
+                 terminate_on_goal: Optional[bool] = None,
+                 max_runtime: Optional[int] = None,
+                 use_data_properties: Optional[str] = 'True',
+                 use_card_restrictions: bool = True, 
+                 use_inverse: bool = False, 
+                 tournament_size: int = 7,
+                 card_limit: int = 10,
+                 population_size: int = 800,
+                 num_generations: int = 200,
+                 height_limit: int = 17):
+        self.evolearner = EvoLearner
         self.binning_value_splitter = BinningValueSplitter()
         self.entropy_value_splitter = EntropyValueSplitter()
         self.knowledge_base = knowledge_base
-        self.quality_func = quality_func
+        self.quality_func = self.transform_quality_func(quality_func)
         self.fitness_func = fitness_func
         self.init_method = init_method
         self.terminate_on_goal = terminate_on_goal
         self.algorithm = algorithm
         self.mut_uniform_gen = mut_uniform_gen
         self.value_splitter = self.transform_value_splitter(value_splitter)
-        self.use_data_properties = use_data_properties
+        self.use_data_properties = self.transform_use_data_properties(use_data_properties)
         self.use_card_restrictions = use_card_restrictions
         self.max_runtime = max_runtime
         self.use_inverse = use_inverse
@@ -33,32 +48,46 @@ class EvoLearnerWrapper:
         self.height_limit = height_limit
 
     def transform_value_splitter(self, value_splitter):
-        if value_splitter == 'EntropyValueSplitter':
+        if value_splitter == 'entropy_value_splitter':
             value_splitter = self.entropy_value_splitter        
-        elif value_splitter == 'BinningValueSplitter':
+        elif value_splitter == 'binning_value_splitter':
             value_splitter = self.binning_value_splitter
         else:
             value_splitter = None
         return value_splitter
         
+    def transform_use_data_properties(self, use_data_properties):
+        if use_data_properties == 'False':
+            use_data_properties = False
+        else:
+            use_data_properties = True
+        return use_data_properties
+    
+    def transform_quality_func(self, quality_func):
+        if quality_func == 'F1':
+            quality_func = F1()
+        else:
+            quality_func = Accuracy()
+        return quality_func
+    
     def get_evolearner_model(self):
-        model = EvoLearner(self.knowledge_base,
-            self.quality_func,
-            self.fitness_func, 
-            self.init_method, 
-            self.algorithm, 
-            self.mut_uniform_gen, 
-            self.value_splitter, 
-            self.terminate_on_goal,
-            self. max_runtime, 
-            self.use_data_properties, 
-            self.use_card_restrictions, 
-            self.use_inverse, 
-            self.tournament_size, 
-            self.card_limit, 
-            self.population_size, 
-            self.num_generations, 
-            self.height_limit)
+        model = self.evolearner(self.knowledge_base,
+                                self.quality_func,
+                                self.fitness_func, 
+                                self.init_method, 
+                                self.algorithm,
+                                self.mut_uniform_gen, 
+                                self.value_splitter, 
+                                self.terminate_on_goal,
+                                self. max_runtime, 
+                                self.use_data_properties, 
+                                self.use_card_restrictions, 
+                                self.use_inverse, 
+                                self.tournament_size, 
+                                self.card_limit, 
+                                self.population_size, 
+                                self.num_generations, 
+                                self.height_limit)
         return model
         
 
