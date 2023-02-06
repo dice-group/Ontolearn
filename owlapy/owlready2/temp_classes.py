50ee010c9a8379970bb648aab2d3b5f98ec4c4c1
LukasBluebaum <lukasbluebaumb94@gmail.com>
2023-02-06 12:56:08 +0100

    Try retrieval of super classes for complex class expressions

--- owlapy/owlready2/temp_classes.py
+++ owlapy/owlready2/temp_classes.py
@@ -6,7 +6,10 @@ import owlready2
 
 from owlapy.model import OWLClass, OWLClassExpression, OWLNamedIndividual, IRI
 from owlapy.owlready2 import OWLReasoner_Owlready2, OWLOntology_Owlready2, BaseReasoner_Owlready2
-from owlapy.owlready2.utils import ToOwlready2
+from owlapy.owlready2.utils import FromOwlready2, ToOwlready2
+
+
+_parse_concept_to_owlapy = FromOwlready2().map_concept
 
 
 class OWLReasoner_Owlready2_TempClasses(OWLReasoner_Owlready2):
@@ -32,7 +35,26 @@ class OWLReasoner_Owlready2_TempClasses(OWLReasoner_Owlready2):
                 temp_pred.equivalent_to = [self._conv.map_concept(ce)]
             self._sync_reasoner(other_reasoner=self._base_reasoner)
             instances = list(temp_pred.instances(world=self._world))
+            # investigate: Need to clear this manually, owlready2 seems to have a bug in destroy_entity in some cases
+            temp_pred.equivalent_to = []
             owlready2.destroy_entity(temp_pred)
             self._cnt += 1
             for i in instances:
                 yield OWLNamedIndividual(IRI.create(i.iri))
+
+    def super_classes(self, ce: OWLClassExpression, direct: bool = False) -> Iterable[OWLClassExpression]:
+        if isinstance(ce, OWLClass):
+            yield from super().super_classes(ce, direct=direct)
+        else:
+            with self._world.get_ontology("http://temp.classes/"):
+                temp_pred = cast(owlready2.ThingClass, types.new_class("TempCls%d" % self._cnt, (owlready2.owl.Thing,)))
+                temp_pred.equivalent_to = [self._conv.map_concept(ce)]
+            self._sync_reasoner(other_reasoner=self._base_reasoner)
+
+            super_classes = temp_pred.is_a if direct else temp_pred.ancestors(include_self=False)
+            for c in super_classes:
+                if isinstance(c, (owlready2.ThingClass, owlready2.ClassConstruct)):
+                    yield _parse_concept_to_owlapy(c)
+            # investigate: Need to clear this manually, owlready2 seems to have a bug in destroy_entity in some cases
+            temp_pred.equivalent_to = []
+            owlready2.destroy_entity(temp_pred)
