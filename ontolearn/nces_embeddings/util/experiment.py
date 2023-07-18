760d26487dccc15003cfd85b4f115e0f22622878
Jean-KOUAGOU <jeank@aims.ac.za>
2023-07-18 13:58:07 +0200

    fixed path not found bug in nces_embeddings

--- ontolearn/nces_embeddings/util/experiment.py
+++ ontolearn/nces_embeddings/util/experiment.py
@@ -270,6 +270,8 @@ class Experiment:
         num_param = sum([p.numel() for p in model.parameters()])
         print("'Number of free parameters: {0}".format(num_param))
         # Store the setting.
+        if not os.path.exists(self.storage_path):
+            os.mkdir(self.storage_path)
         with open(self.storage_path + '/settings.json', 'w') as file_descriptor:
             json.dump(self.kwargs, file_descriptor)
 
