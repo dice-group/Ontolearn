d9de57f08395e6c771fbd6969ce3ecb56096514b
Caglar Demir <caglardemir8@gmail.com>
2023-12-15 17:41:22 +0100

    Update tdl_stratified_kfold_cv_experiments.sh
    Typo fixed in the runtime

--- tdl_stratified_kfold_cv_experiments.sh
+++ tdl_stratified_kfold_cv_experiments.sh
@@ -1,6 +1,6 @@
 mkdir CVFamilyBenchmarkResults
-python examples/concept_learning_cv_evaluation.py --lps LPs/Family/lps.json --kb KGs/Family/family.owl --max_runtime 1 --report cv_family_results.csv && mv cv_family_results.csv CVFamilyBenchmarkResults
+python examples/concept_learning_cv_evaluation.py --lps LPs/Family/lps.json --kb KGs/Family/family.owl --max_runtime 60 --report cv_family_results.csv && mv cv_family_results.csv CVFamilyBenchmarkResults
 mkdir CVMutagenesisBenchmarkResults
-python examples/concept_learning_cv_evaluation.py --lps LPs/Mutagenesis/lps.json --kb KGs/Mutagenesis/mutagenesis.owl --max_runtime 60 --report cv_mutagenesis_results1.csv && mv cv_mutagenesis_results1.csv CVMutagenesisBenchmarkResults
+python examples/concept_learning_cv_evaluation.py --lps LPs/Mutagenesis/lps.json --kb KGs/Mutagenesis/mutagenesis.owl --max_runtime 60 --report cv_mutagenesis_results.csv && mv cv_mutagenesis_results.csv CVMutagenesisBenchmarkResults
 mkdir CVCarcinogenesisBenchmarkResults
-python examples/concept_learning_cv_evaluation.py --lps LPs/Carcinogenesis/lps.json --kb KGs/Carcinogenesis/carcinogenesis.owl --max_runtime 60 --report cv_carcinogenesis_results1.csv && mv cv_carcinogenesis_results1.csv CVCarcinogenesisBenchmarkResults
+python examples/concept_learning_cv_evaluation.py --lps LPs/Carcinogenesis/lps.json --kb KGs/Carcinogenesis/carcinogenesis.owl --max_runtime 60 --report cv_carcinogenesis_results.csv && mv cv_carcinogenesis_results.csv CVCarcinogenesisBenchmarkResults
