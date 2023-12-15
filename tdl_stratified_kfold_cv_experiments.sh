63c6aafd4050cdb04496dc391b4d76fc34a81272
Caglar Demir <caglardemir8@gmail.com>
2023-12-15 17:39:43 +0100

    WIP: Eval tDL

--- tdl_stratified_kfold_cv_experiments.sh
+++ tdl_stratified_kfold_cv_experiments.sh
@@ -1,8 +1,6 @@
-
 mkdir CVFamilyBenchmarkResults
-python examples/concept_learning_cv_evaluation.py --lps LPs/Family/lps.json --kb KGs/Family/family.owl --max_runtime 1 --report family_results1.csv && mv family_results1.csv CVFamilyBenchmarkResults
-
-#mkdir CVMutagenesisBenchmarkResults
-#python examples/concept_learning_cv_evaluation.py --lps LPs/Mutagenesis/lps.json --kb KGs/Mutagenesis/mutagenesis.owl --max_runtime 60 --report mutagenesis_results1.csv && mv mutagenesis_results1.csv CVMutagenesisBenchmarkResults
-#mkdir CVCarcinogenesisBenchmarkResults
-#python examples/concept_learning_cv_evaluation.py --lps LPs/Carcinogenesis/lps.json --kb KGs/Carcinogenesis/carcinogenesis.owl --max_runtime 60 --report carcinogenesis_results1.csv && mv carcinogenesis_results1.csv CVCarcinogenesisBenchmarkResults
+python examples/concept_learning_cv_evaluation.py --lps LPs/Family/lps.json --kb KGs/Family/family.owl --max_runtime 1 --report cv_family_results.csv && mv cv_family_results.csv CVFamilyBenchmarkResults
+mkdir CVMutagenesisBenchmarkResults
+python examples/concept_learning_cv_evaluation.py --lps LPs/Mutagenesis/lps.json --kb KGs/Mutagenesis/mutagenesis.owl --max_runtime 60 --report cv_mutagenesis_results1.csv && mv cv_mutagenesis_results1.csv CVMutagenesisBenchmarkResults
+mkdir CVCarcinogenesisBenchmarkResults
+python examples/concept_learning_cv_evaluation.py --lps LPs/Carcinogenesis/lps.json --kb KGs/Carcinogenesis/carcinogenesis.owl --max_runtime 60 --report cv_carcinogenesis_results1.csv && mv cv_carcinogenesis_results1.csv CVCarcinogenesisBenchmarkResults
