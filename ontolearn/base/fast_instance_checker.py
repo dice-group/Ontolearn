1430c2ae4481ab2134b884a011e274f6a94f94dc
Alkid <alkid1baci@gmail.com>
2024-01-30 16:59:01 +0100

    Added method for accessing super properties

--- ontolearn/base/fast_instance_checker.py
+++ ontolearn/base/fast_instance_checker.py
@@ -6,8 +6,9 @@ import owlready2
 from functools import singledispatchmethod, reduce
 from itertools import repeat, chain
 from types import MappingProxyType, FunctionType
-from typing import DefaultDict, Iterable, Dict, Mapping, Set, Type, TypeVar, Optional, FrozenSet
+from typing import DefaultDict, Iterable, Dict, Mapping, Set, Type, TypeVar, Optional, FrozenSet, cast
 
+from ontolearn.base import OWLReasoner_Owlready2
 from ontolearn.base.ext import OWLReasonerEx
 from owlapy.model import OWLDataRange, OWLObjectOneOf, OWLOntology, OWLNamedIndividual, OWLClass, \
     OWLObjectProperty, OWLDataProperty, OWLObjectUnionOf, OWLObjectIntersectionOf, OWLObjectSomeValuesFrom, \
@@ -186,6 +187,13 @@ class OWLReasoner_FastInstanceChecker(OWLReasonerEx):
 
     def sub_data_properties(self, dp: OWLDataProperty, direct: bool = False) -> Iterable[OWLDataProperty]:
         yield from self._base_reasoner.sub_data_properties(dp=dp, direct=direct)
+
+    # the cast to OWLReasoner_Owlready2 is temporary. After new release of owlapy it will not be needed anymore.
+    def super_data_properties(self, dp: OWLDataProperty, direct: bool = False) -> Iterable[OWLDataProperty]:
+        yield from cast(OWLReasoner_Owlready2, self._base_reasoner).super_data_properties(dp=dp, direct=direct)
+
+    def super_object_properties(self, op: OWLObjectProperty, direct: bool = False) -> Iterable[OWLDataProperty]:
+        yield from cast(OWLReasoner_Owlready2, self._base_reasoner).super_object_properties(op=op, direct=direct)
 
     def sub_object_properties(self, op: OWLObjectPropertyExpression, direct: bool = False) \
             -> Iterable[OWLObjectPropertyExpression]:
