afed7f3300e27e73a8b3336b88831fe75e916d4f
Luke Friedrichs <lukefriedrichs@gmail.com>
2024-05-07 16:21:36 +0200

    Neural Reasoner get types individuals demo method

--- test_neural_reasoner.py
+++ test_neural_reasoner.py
@@ -1,23 +1,28 @@
 from ontolearn.triple_store import TripleStore, TripleStoreReasonerOntology
+from dicee import KGE
+from owlapy.class_expression import OWLClass
 
 
 class DemoNeuralReasoner:
+    model: KGE
+
     def __init__(self, KGE_path: str):
-        pass
+        self.model = KGE(path=KGE_path)
 
-    def get_type_individuals(self, individual: str):
-        return set()
-
-
-kb_classic = TripleStore(url="http://localhost:3030/father")
-
-
-for t in kb_classic.g.get_type_individuals("http://example.com/father#anna"):
-    print(t)
+    def get_type_individuals(self, individual: str, confidence_threshold: float = 0.9):
+        predictions = self.model.predict_topk(
+            h=[individual],
+            r=["http://www.w3.org/1999/02/22-rdf-syntax-ns#type"],
+            t=None,
+            topk=10,
+        )
+        for prediction in predictions:
+            print(prediction[1])
+            yield OWLClass(prediction[0])
 
 
 print("###########################")
 
-kb_neural = TripleStore(DemoNeuralReasoner())
+kb_neural = TripleStore(DemoNeuralReasoner(KGE_path="./Pykeen_QuateEFatherRun"))
 for t in kb_neural.g.get_type_individuals("http://example.com/father#anna"):
     print(t)
