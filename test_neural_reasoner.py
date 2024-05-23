0925d68546cc99bd08a68ad0bbc925d0206351b1
Luke Friedrichs <lukefriedrichs@gmail.com>
2024-05-23 17:21:52 +0200

    neural evaluation script

--- test_neural_reasoner.py
+++ test_neural_reasoner.py
@@ -13,6 +13,10 @@ from ontolearn.triple_store import TripleStore
 from ontolearn.learning_problem import PosNegLPStandard
 from owlapy.owl_individual import OWLNamedIndividual
 from owlapy import owl_expression_to_sparql, owl_expression_to_dl
+import sys
+
+
+sys.setrecursionlimit(10000)
 
 
 class DemoNeuralReasoner:
@@ -60,10 +64,11 @@ class DemoNeuralReasoner:
         None,
         None,
     ]:
+        subject_ = OWLNamedIndividual(str_iri)
         # for p == type
         for cl in self.get_type_individuals(str_iri):
             yield (
-                OWLNamedIndividual(str_iri),
+                subject_,
                 OWLProperty("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"),
                 cl,
             )
@@ -71,12 +76,13 @@ class DemoNeuralReasoner:
         # for p == object property
         for op in self.object_properties_in_signature():
             for o in self.get_object_property_values(str_iri, op):
-                yield (OWLNamedIndividual(str_iri), op, o)
+                yield (subject_, op, o)
 
         # for p == data property
         for dp in self.data_properties_in_signature():
+            print("these data properties are in the signature: ", dp.str)
             for l in self.get_data_property_values(str_iri, dp):
-                yield (OWLNamedIndividual(str_iri), dp, l)
+                yield (subject_, dp, l)
 
     def classes_in_signature(
         self, confidence_threshold: float = None
@@ -220,96 +226,106 @@ class DemoNeuralReasoner:
 # sudo docker run -p 3030:3030 -e ADMIN_PASSWORD=pw123 stain/jena-fuseki
 # Login http://localhost:3030/#/ with admin and pw123
 # Create a new dataset called family and upload KGs/Family/family.owl
-# kb = TripleStore(DemoNeuralReasoner(KGE_path="./Pykeen_QuatEFamilyRun"))
-kb = TripleStore(DemoNeuralReasoner(KGE_path="./Pykeen_QuatEFamilyRun"))
+
+# kb = TripleStore(url="http://localhost:3030/family")
+kb = TripleStore(DemoNeuralReasoner(KGE_path="./embeddings/Pykeen_QuatEFamilyRun"))
+for dp in kb.g.data_properties_in_signature():
+    print(dp)
+
+assert True == False
 # (2) Initialize a learner.
 model = TDL(knowledge_base=kb)
 # (3) Define a description logic concept learning problem.
 lp = PosNegLPStandard(
     pos={
-        OWLNamedIndividual("http://www.benchmark.org/family#F5M64"),
-        OWLNamedIndividual("http://www.benchmark.org/family#F6M92"),
-        OWLNamedIndividual("http://www.benchmark.org/family#F9M157"),
-        OWLNamedIndividual("http://www.benchmark.org/family#F6M71"),
-        OWLNamedIndividual("http://www.benchmark.org/family#F2M11"),
-        OWLNamedIndividual("http://www.benchmark.org/family#F3M45"),
-        OWLNamedIndividual("http://www.benchmark.org/family#F7M123"),
-    },
-    neg={
-        OWLNamedIndividual("http://www.benchmark.org/family#F5M66"),
-        OWLNamedIndividual("http://www.benchmark.org/family#F10M188"),
-        OWLNamedIndividual("http://www.benchmark.org/family#F10M173"),
-        OWLNamedIndividual("http://www.benchmark.org/family#F1F3"),
-        OWLNamedIndividual("http://www.benchmark.org/family#F2M37"),
-        OWLNamedIndividual("http://www.benchmark.org/family#F7M115"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F2F14"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F2F12"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F2F19"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F2F26"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F2F28"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F2F36"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F3F52"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F3F53"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F5F62"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F6F72"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F6F79"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F6F77"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F6F86"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F6F91"),
         OWLNamedIndividual("http://www.benchmark.org/family#F6F84"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F6F96"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F6F101"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F6F93"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F7F114"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F7F106"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F7F116"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F7F119"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F7F126"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F7F121"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F9F148"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F9F150"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F9F143"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F9F152"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F9F154"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F9F141"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F9F160"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F9F163"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F9F158"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F9F168"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F10F174"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F10F179"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F10F181"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F10F192"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F10F193"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F10F186"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F10F195"),
     },
-)
-# (4) Learn description logic concepts best fitting (3).
-h = model.fit(learning_problem=lp).best_hypotheses()
-print(h)
-print(owl_expression_to_dl(h))
-print(owl_expression_to_sparql(expression=h))
-
-
-# (1) Initialize Triplestore
-# sudo docker run -p 3030:3030 -e ADMIN_PASSWORD=pw123 stain/jena-fuseki
-# Login http://localhost:3030/#/ with admin and pw123
-# Create a new dataset called family and upload KGs/Family/family.owl
-kb = TripleStore(DemoNeuralReasoner(KGE_path="./Pykeen_QuatEFatherRun"))
-# (2) Initialize a learner.
-model = TDL(knowledge_base=kb)
-# (3) Define a description logic concept learning problem.
-lp = PosNegLPStandard(
-    pos={OWLNamedIndividual("http://example.com/father#stefan")},
     neg={
-        OWLNamedIndividual("http://example.com/father#heinz"),
-        OWLNamedIndividual("http://example.com/father#anna"),
-        OWLNamedIndividual("http://example.com/father#michelle"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F6M99"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F10F200"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F9F156"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F6M69"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F2F15"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F6M100"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F8F133"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F3F48"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F2F30"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F4F55"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F6F74"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F10M199"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F7M104"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F9M146"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F6M71"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F2F22"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F2M13"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F9F169"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F5F65"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F6M81"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F7M131"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F7F129"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F7M107"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F10F189"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F8F135"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F8M136"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F10M188"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F9F164"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F7F118"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F2F10"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F6F97"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F7F111"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F9M151"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F4M59"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F2M37"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F1M1"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F9M142"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F4M57"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F9M170"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F5M66"),
+        OWLNamedIndividual("http://www.benchmark.org/family#F9F145"),
     },
 )
 # (4) Learn description logic concepts best fitting (3).
 h = model.fit(learning_problem=lp).best_hypotheses()
-print("------------------- Neural Reasoner lp results -------------------")
 print(h)
 print(owl_expression_to_dl(h))
 print(owl_expression_to_sparql(expression=h))
-
-print("------------------- Classic Reasoner -------------------")
-
-kb_classic = TripleStore(url="http://localhost:3030/father")
-
-for t in kb_classic.g.get_type_individuals("http://example.com/father#anna"):
-    print(t)
-
-print("------------------- instances function -------------------")
-for i in kb_classic.g.instances(OWLClass("http://example.com/father#female")):
-    print(i)
-
-
-print("+++++++++++++++++++ Neural Reasoner +++++++++++++++++++")
-
-kb_neural = TripleStore(DemoNeuralReasoner(KGE_path="./Pykeen_QuatEFatherRun"))
-for t in kb_neural.g.get_type_individuals("http://example.com/father#anna"):
-    print(t)
-
-print("+++++++++++++++++++ instances function +++++++++++++++++++")
-for i in kb_neural.g.instances(OWLClass("http://example.com/father#female")):
-    print(i)
-
-print("+++++++++++++++++++ classes_in_signature function +++++++++++++++++++")
-for c in kb_neural.g.classes_in_signature():
-    print(c)
-
-print("+++++++++++++++++++ data_properties_in_signature function +++++++++++++++++++")
-for dp in kb_neural.g.data_properties_in_signature():
-    print(dp)
-
-print("+++++++++++++++++++ abox function +++++++++++++++++++")
-for a in kb_neural.g.abox("http://example.com/father#anna"):
-    print(a)
-
-
-print("+++++++++++++++++++ object_properties_in_signature function +++++++++++++++++++")
-for op in kb_neural.g.object_properties_in_signature():
-    print(op)
