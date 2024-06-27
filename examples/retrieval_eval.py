e2df458ecb2f544d344aed1e3c1954caf0d0f18d
Luke Friedrichs <lukefriedrichs@gmail.com>
2024-06-27 11:55:40 +0200

    retrieval eval script extended

--- examples/retrieval_eval.py
+++ examples/retrieval_eval.py
@@ -35,7 +35,7 @@ def concept_reducer(concepts, opt):
     return result
 
 
-def concept_reducer_properties(concepts, opt, cardinality=5):
+def concept_reducer_properties(concepts, opt, cardinality=2):
     result = set()
     for i in concepts:
         for j in object_properties_and_inverse:
@@ -59,13 +59,14 @@ def retrieval_eval(expressions, y, yhat, verbose=1):
     assert len(y) == len(yhat)
     similarities = []
     runtime_diff = []
+    number_of_concepts = len(expressions)
     for expressions, y_report_i, yhat_report_i in zip(expressions, y, yhat):
         runtime_y_i, y_i = y_report_i
         runtime_yhat_i, yhat_i = yhat_report_i
 
         jaccard_sim = jaccard_similarity(y_i, yhat_i)
         runtime_benefits = runtime_y_i - runtime_yhat_i
-        if verbose > 0:
+        if verbose > 1:  # todo: set to 0
             print(
                 f"Concept:{expressions}\tTrue Size:{len(y_i)}\tPredicted Size:{len(yhat_i)}\tRetrieval Similarity:{jaccard_sim}\tRuntime Benefit:{runtime_benefits:.3f}"
             )
@@ -73,7 +74,7 @@ def retrieval_eval(expressions, y, yhat, verbose=1):
         runtime_diff.append(runtime_benefits)
     avg_jaccard_sim = sum(similarities) / len(similarities)
     avg_runtime_benefits = sum(runtime_diff) / len(runtime_diff)
-    return avg_jaccard_sim, avg_runtime_benefits
+    return number_of_concepts, avg_jaccard_sim, avg_runtime_benefits
 
 
 unions = concept_reducer(nc, opt=OWLObjectUnionOf)
@@ -82,8 +83,16 @@ unions_unnc = concept_reducer(unnc, opt=OWLObjectUnionOf)
 intersections_unnc = concept_reducer(unnc, opt=OWLObjectIntersectionOf)
 exist_unnc = concept_reducer_properties(unnc, opt=OWLObjectSomeValuesFrom)
 for_all_unnc = concept_reducer_properties(unnc, opt=OWLObjectAllValuesFrom)
-min_cardinality_unnc = concept_reducer_properties(unnc, opt=OWLObjectMinCardinality)
-max_cardinality_unnc = concept_reducer_properties(unnc, opt=OWLObjectMaxCardinality)
+
+min_cardinality_unnc_1, min_cardinality_unnc_2, min_cardinality_unnc_3 = (
+    concept_reducer_properties(unnc, opt=OWLObjectMinCardinality, cardinality=i)
+    for i in [1, 2, 3]
+)
+max_cardinality_unnc_1, max_cardinality_unnc_2, max_cardinality_unnc_3 = (
+    concept_reducer_properties(unnc, opt=OWLObjectMaxCardinality, cardinality=i)
+    for i in [1, 2, 3]
+)
+
 nc_retrieval_results = retrieval_eval(
     expressions=nc,
     y=concept_to_retrieval(nc, symbolic_kb),
@@ -129,15 +138,39 @@ for_all_unnc_retrieval_results = retrieval_eval(
     y=concept_to_retrieval(for_all_unnc, symbolic_kb),
     yhat=concept_to_retrieval(for_all_unnc, neural_owl_reasoner),
 )
-min_cardinality_unnc_retrieval_results = retrieval_eval(
-    expressions=min_cardinality_unnc,
-    y=concept_to_retrieval(min_cardinality_unnc, symbolic_kb),
-    yhat=concept_to_retrieval(min_cardinality_unnc, neural_owl_reasoner),
+
+(
+    min_cardinality_unnc_1_retrieval_results,
+    min_cardinality_unnc_2_retrieval_results,
+    min_cardinality_unnc_3_retrieval_results,
+) = (
+    retrieval_eval(
+        expressions=expressions,
+        y=concept_to_retrieval(expressions, symbolic_kb),
+        yhat=concept_to_retrieval(expressions, neural_owl_reasoner),
+    )
+    for expressions in [
+        min_cardinality_unnc_1,
+        min_cardinality_unnc_2,
+        min_cardinality_unnc_3,
+    ]
 )
-max_cardinality_unnc_retrieval_results = retrieval_eval(
-    expressions=max_cardinality_unnc,
-    y=concept_to_retrieval(max_cardinality_unnc, symbolic_kb),
-    yhat=concept_to_retrieval(max_cardinality_unnc, neural_owl_reasoner),
+
+(
+    max_cardinality_unnc_1_retrieval_results,
+    max_cardinality_unnc_2_retrieval_results,
+    max_cardinality_unnc_3_retrieval_results,
+) = (
+    retrieval_eval(
+        expressions=expressions,
+        y=concept_to_retrieval(expressions, symbolic_kb),
+        yhat=concept_to_retrieval(expressions, neural_owl_reasoner),
+    )
+    for expressions in [
+        max_cardinality_unnc_1,
+        max_cardinality_unnc_2,
+        max_cardinality_unnc_3,
+    ]
 )
 
 
@@ -151,19 +184,27 @@ results = {
     "intersections_unnc_retrieval_results": intersections_unnc_retrieval_results,
     "exist_unnc_retrieval_results": exist_unnc_retrieval_results,
     "for_all_unnc_retrieval_results": for_all_unnc_retrieval_results,
-    "min_cardinality_unnc_retrieval_results": min_cardinality_unnc_retrieval_results,
-    "max_cardinality_unnc_retrieval_results": max_cardinality_unnc_retrieval_results,
+    "min_cardinality_unnc_1_retrieval_results": min_cardinality_unnc_1_retrieval_results,
+    "min_cardinality_unnc_2_retrieval_results": min_cardinality_unnc_2_retrieval_results,
+    "min_cardinality_unnc_3_retrieval_results": min_cardinality_unnc_3_retrieval_results,
+    "max_cardinality_unnc_1_retrieval_results": max_cardinality_unnc_1_retrieval_results,
+    "max_cardinality_unnc_2_retrieval_results": max_cardinality_unnc_2_retrieval_results,
+    "max_cardinality_unnc_3_retrieval_results": max_cardinality_unnc_3_retrieval_results,
 }
 
 
 # logger that prints the results
 def print_results(results):
+    print(f"Number of named and negated named concepts: {len(unnc)}")
+    print(
+        f"Number of object properties and their inverses: {len(object_properties_and_inverse)}"
+    )
+    print("\n")
+    print("(Number of Concepts, Jackard Similarity, Runtime Benefits)")
     for k, v in results.items():
         print("\n")
         print(f"{k}:")
         print(v)
 
 
-print("\n")
-print("(Jackard Similarity, Runtime Benefits)")
 print_results(results)
