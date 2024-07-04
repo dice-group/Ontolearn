f030519cc770b1f0e37f7a93b68eac2d992c57b1
Caglar Demir <caglardemir8@gmail.com>
2024-07-04 15:16:02 +0200

    Retrieval refactored

--- examples/retrieval_eval.py
+++ examples/retrieval_eval.py
@@ -1,6 +1,9 @@
+"""python examples/retrieval_eval.py"""
 from ontolearn.owl_neural_reasoner import TripleStoreNeuralReasoner
 from ontolearn.knowledge_base import KnowledgeBase
+from ontolearn.triple_store import TripleStore
 from ontolearn.utils import jaccard_similarity
+from owlapy.class_expression import OWLQuantifiedObjectRestriction, OWLObjectCardinalityRestriction
 from owlapy.class_expression import (
     OWLObjectUnionOf,
     OWLObjectIntersectionOf,
@@ -10,24 +13,15 @@ from owlapy.class_expression import (
     OWLObjectMaxCardinality,
 )
 import time
-from typing import List, Tuple, Set
-
-symbolic_kb = KnowledgeBase(path="KGs/Family/family-benchmark_rich_background.owl")
-# symbolic_kb = TripleStore(url="http://localhost:3030/family")
-neural_owl_reasoner = TripleStoreNeuralReasoner(
-    path_of_kb="KGs/Family/family-benchmark_rich_background.owl", gamma=0.8
-)
-object_properties = {i for i in symbolic_kb.get_object_properties()}
-object_properties_inverse = {i.get_inverse_property() for i in object_properties}
-object_properties_and_inverse = object_properties.union(object_properties_inverse)
-# named concepts
-nc = {i for i in symbolic_kb.get_concepts()}
-# negated named concepts
-nnc = {i.get_object_complement_of() for i in nc}
-# union of named and negated named concepts
-unnc = nc.union(nnc)
+from typing import List, Tuple, Union, Set, Iterable, Callable
+import pandas as pd
+from owlapy import owl_expression_to_dl
+from itertools import chain
+from argparse import ArgumentParser
+import os
 
 
+# @TODO Move into ontolearn.utils
 def concept_reducer(concepts, opt):
     result = set()
     for i in concepts:
@@ -36,17 +30,38 @@ def concept_reducer(concepts, opt):
     return result
 
 
-def concept_reducer_properties(concepts, opt, cardinality=2):
+# @TODO Move into ontolearn.utils
+def concept_reducer_properties(concepts: Set,
+                               properties, cls: Callable = None,
+                               cardinality: int = 2) -> Set[
+    Union[OWLQuantifiedObjectRestriction, OWLObjectCardinalityRestriction]]:
+    """
+    Map a set of owl concepts and a set of properties into OWL Restrictions
+
+    Args:
+        concepts:
+        properties:
+        cls (Callable): An owl Restriction class
+        cardinality: A positive Integer
+
+    Returns: List of OWL Restrictions
+
+    """
+    assert isinstance(concepts, Iterable), "Concepts must be an Iterable"
+    assert isinstance(properties, Iterable), "properties must be an Iterable"
+    assert isinstance(cls, Callable), "cls must be an Callable"
+    assert cardinality > 0
     result = set()
     for i in concepts:
-        for j in object_properties_and_inverse:
-            if opt == OWLObjectMinCardinality or opt == OWLObjectMaxCardinality:
-                result.add(opt(cardinality, j, i))
+        for j in properties:
+            if cls == OWLObjectMinCardinality or cls == OWLObjectMaxCardinality:
+                result.add(cls(cardinality=cardinality, property=j, filler=i))
                 continue
-            result.add(opt(j, i))
+            result.add(cls(j, i))
     return result
 
 
+# @TODO: CD: Perhaps we can remove this function.
 def concept_to_retrieval(concepts, retriever) -> List[Tuple[float, Set[str]]]:
     results = []
     for c in concepts:
@@ -56,6 +71,7 @@ def concept_to_retrieval(concepts, retriever) -> List[Tuple[float, Set[str]]]:
     return results
 
 
+# @TODO: CD: Perhaps we can remove this function.
 def retrieval_eval(expressions, y, yhat, verbose=1):
     assert len(y) == len(yhat)
     similarities = []
@@ -78,27 +94,116 @@ def retrieval_eval(expressions, y, yhat, verbose=1):
     return number_of_concepts, avg_jaccard_sim, avg_runtime_benefits
 
 
-unions = concept_reducer(nc, opt=OWLObjectUnionOf)
-intersections = concept_reducer(nc, opt=OWLObjectIntersectionOf)
-unions_unnc = concept_reducer(unnc, opt=OWLObjectUnionOf)
-intersections_unnc = concept_reducer(unnc, opt=OWLObjectIntersectionOf)
-exist_unnc = concept_reducer_properties(unnc, opt=OWLObjectSomeValuesFrom)
-for_all_unnc = concept_reducer_properties(unnc, opt=OWLObjectAllValuesFrom)
+def execute(args):
+    # (1) Initialize knowledge base.
+    assert os.path.isfile(args.path_kg)
+    if args.endpoint_triple_store:
+        symbolic_kb = TripleStore(url="http://localhost:3030/family")
+    else:
+        symbolic_kb = KnowledgeBase(path=args.path_kg)
+    # (2) Initialize Neural OWL Reasoner.
+    neural_owl_reasoner = TripleStoreNeuralReasoner(path_of_kb=args.path_kg, gamma=args.gamma)
+    ###################################################################
+    # GENERATE ALCQ CONCEPTS TO EVALUATE RETRIEVAL PERFORMANCES
+    # (3) R: Extract object properties.
+    object_properties = {i for i in symbolic_kb.get_object_properties()}
+    # (4) R⁻: Inverse of object properties.
+    object_properties_inverse = {i.get_inverse_property() for i in object_properties}
+    # (5) R*: R UNION R⁻.
+    object_properties_and_inverse = object_properties.union(object_properties_inverse)
+    # (6) NC: Named owl concepts.
+    nc = {i for i in symbolic_kb.get_concepts()}
+    # (7) NC⁻: Complement of NC.
+    nnc = {i.get_object_complement_of() for i in nc}
+    # (8) UNNC: NC UNION NC⁻.
+    unnc = nc.union(nnc)
+    # (9) NC UNION NC.
+    unions = concept_reducer(nc, opt=OWLObjectUnionOf)
+    # (10) NC INTERSECTION NC.
+    intersections = concept_reducer(nc, opt=OWLObjectIntersectionOf)
+    # (11) UNNC UNION UNNC.
+    unions_unnc = concept_reducer(unnc, opt=OWLObjectUnionOf)
+    # (12) UNNC INTERACTION UNNC.
+    intersections_unnc = concept_reducer(unnc, opt=OWLObjectIntersectionOf)
 
-min_cardinality_unnc_1, min_cardinality_unnc_2, min_cardinality_unnc_3 = (
-    concept_reducer_properties(unnc, opt=OWLObjectMinCardinality, cardinality=i)
-    for i in [1, 2, 3]
-)
-max_cardinality_unnc_1, max_cardinality_unnc_2, max_cardinality_unnc_3 = (
-    concept_reducer_properties(unnc, opt=OWLObjectMaxCardinality, cardinality=i)
-    for i in [1, 2, 3]
-)
+    # (13) \exist r. C s.t. C \in UNNC and r \in R* .
+    exist_unnc = concept_reducer_properties(concepts=unnc,
+                                            properties=object_properties_and_inverse,
+                                            cls=OWLObjectSomeValuesFrom)
+    # (15) \forall r. C s.t. C \in UNNC and r \in R* .
+    for_all_unnc = concept_reducer_properties(concepts=unnc,
+                                              properties=object_properties_and_inverse,
+                                              cls=OWLObjectAllValuesFrom)
+    # (16) >= n r. C  and =< n r. C, s.t. C \in UNNC and r \in R* .
+    min_cardinality_unnc_1, min_cardinality_unnc_2, min_cardinality_unnc_3 = (
+        concept_reducer_properties(concepts=unnc, properties=object_properties_and_inverse, cls=OWLObjectMinCardinality,
+                                   cardinality=i)
+        for i in [1, 2, 3]
+    )
+    max_cardinality_unnc_1, max_cardinality_unnc_2, max_cardinality_unnc_3 = (
+        concept_reducer_properties(concepts=unnc,
+                                   properties=object_properties_and_inverse,
+                                   cls=OWLObjectMaxCardinality,
+                                   cardinality=i)
+        for i in [1, 2, 3]
+    )
 
+    ###################################################################
+
+    ## RETRIEVAL RESULTS
+
+    def concept_retrieval(retriever_func, c) -> Tuple[Set[str], float]:
+        start_time = time.time()
+        return {i.str for i in retriever_func.individuals(c)}, time.time() - start_time
+
+    data = []
+
+    for i in chain(nc, unions, intersections,
+                   nnc, unnc, unions_unnc, intersections_unnc,
+                   exist_unnc, for_all_unnc,
+                   min_cardinality_unnc_1, min_cardinality_unnc_2, min_cardinality_unnc_3,
+                   max_cardinality_unnc_1, max_cardinality_unnc_2, max_cardinality_unnc_3):
+        retrieval_y, runtime_y = concept_retrieval(symbolic_kb, i)
+        retrieval_neural_y, runtime_neural_y = concept_retrieval(neural_owl_reasoner, i)
+        jaccard_sim = jaccard_similarity(retrieval_y, retrieval_neural_y)
+        data.append({"Expression": owl_expression_to_dl(i),
+                     "Type": type(i).__name__,
+                     "Jaccard Similarity": jaccard_sim,
+                     "Runtime Benefits": runtime_y - runtime_neural_y
+                     })
+
+    df = pd.DataFrame(data)
+    assert df["Jaccard Similarity"].mean() == 1.0
+
+    df.to_csv(args.path_report)
+    del df
+    df=pd.read_csv(args.path_report,index_col=0)
+    numerical_df = df.select_dtypes(include=['number'])
+    df_g = df.groupby(by="Type")
+    print(df_g["Type"].count())
+    mean_df = df_g[numerical_df.columns].mean()
+    print(mean_df)
+
+
+def get_default_arguments():
+    parser = ArgumentParser()
+    parser.add_argument("--path_kg", type=str, default="KGs/Family/family-benchmark_rich_background.owl")
+    parser.add_argument("--endpoint_triple_store", type=str, default=None)
+    parser.add_argument("--gamma", type=float, default=0.8)
+    parser.add_argument("--path_report", type=str, default="ALCQ_Retrieval_Results.csv")
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    execute(get_default_arguments())
+# @TODO:CD:I guess we can remove the below part. What do you think Luke ?
+"""
 nc_retrieval_results = retrieval_eval(
     expressions=nc,
     y=concept_to_retrieval(nc, symbolic_kb),
     yhat=concept_to_retrieval(nc, neural_owl_reasoner),
 )
+
 unions_nc_retrieval_results = retrieval_eval(
     expressions=unions,
     y=concept_to_retrieval(unions, symbolic_kb),
@@ -151,10 +256,10 @@ for_all_unnc_retrieval_results = retrieval_eval(
         yhat=concept_to_retrieval(expressions, neural_owl_reasoner),
     )
     for expressions in [
-        min_cardinality_unnc_1,
-        min_cardinality_unnc_2,
-        min_cardinality_unnc_3,
-    ]
+    min_cardinality_unnc_1,
+    min_cardinality_unnc_2,
+    min_cardinality_unnc_3,
+]
 )
 
 (
@@ -168,12 +273,11 @@ for_all_unnc_retrieval_results = retrieval_eval(
         yhat=concept_to_retrieval(expressions, neural_owl_reasoner),
     )
     for expressions in [
-        max_cardinality_unnc_1,
-        max_cardinality_unnc_2,
-        max_cardinality_unnc_3,
-    ]
+    max_cardinality_unnc_1,
+    max_cardinality_unnc_2,
+    max_cardinality_unnc_3,
+]
 )
-
 
 results = {
     "nc_retrieval_results": nc_retrieval_results,
@@ -201,7 +305,7 @@ def print_results(results):
         f"Number of object properties and their inverses: {len(object_properties_and_inverse)}"
     )
     print("\n")
-    print("(Number of Concepts, Jackard Similarity, Runtime Benefits)")
+    print("(Number of Concepts, Jaccard Similarity, Runtime Benefits)")
     for k, v in results.items():
         print("\n")
         print(f"{k}:")
@@ -209,3 +313,4 @@ def print_results(results):
 
 
 print_results(results)
+"""
