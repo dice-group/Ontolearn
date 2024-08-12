a52ae8161a0f70a339cbaa59f5b671b69727aa0b
Louis-Mozart <louismozart.teyou@aims.ac.rw>
2024-08-12 17:31:27 +0200

    WIP: First working evaluation version

--- examples/retrieval_eval_under_incomplete.py
+++ examples/retrieval_eval_under_incomplete.py
@@ -5,31 +5,85 @@ from argparse import ArgumentParser
 from ontolearn.knowledge_base import KnowledgeBase
 import pandas as pd
 from typing import Set
-
+from incomplete_kb import *
+import os
+from ontolearn.utils import jaccard_similarity
 import subprocess
+from owlapy.class_expression import *
+from owlapy.iri import IRI
+from owlapy.parser import DLSyntaxParser
+import ast
 
 
 # [] Create sub/incomplete KGs
-def generated_incomplete_kg(path: str, directory: str, n: int, ratio: float) -> Set[str]:
+def generated_incomplete_kg(kb_path: str, directory: str, n: int, ratio: float) -> Set[str]:
+
     # (1)
     # TODO:CD: Ensure that randomness can be controlled via seed
     # TODO:CD: Return a set of strings where each item corresponds ot the local path of a sub kg.
-    pass
 
+    #e.g. of how the file can be save
+    # kb_path = "KGs/Family/father.owl"  
+    # output_path = f"incomplete_father_ratio_10_number_1.owl" 
+
+    name = kb_path.split('/')[-1].split('.')[0]
+    rate = int(ratio * 100)
+
+    os.makedirs(directory, exist_ok=True)
+
+    file_paths = set()
+
+    for i in range(1, n + 1):
+
+        # Construct the output path for the incomplete KG
+        output_path = f'{directory}/incomplete_{name}_ratio_{rate}_number_{i}.owl'
+
+        # function to generate the incomplete KG
+        make_kb_incomplete(kb_path, output_path, rate, seed=i)
+
+        # Add the output path to the set
+        file_paths.add(output_path)
+
+    # Return the set of file paths
+    return file_paths
+
+# kb_path = "KGs/Family/father.owl"  
+# directory = "incomplete_father" 
+# u,v = generated_incomplete_kg(kb_path , directory , 2, 0.1)
+# print(u,v)
 
 def execute(args):
+
+    symbolic_kb = KnowledgeBase(path=args.path_kg)
+    # print({i.str for i in symbolic_kb.individuals(OWLClass(IRI('http://example.com/fatherr#','female')))})
+    # exit(0)
     # TODO: Report the results in a CSV file as we have done it in retieval_eval.py
     # Load the full KG
-    symbolic_kb = KnowledgeBase(path=args.path_kg)
-    # TODO: What should be directory args.path_kg?
-    paths_of_incomplete_kgs = generated_incomplete_kg(path=args.path_kg, directory="", n=10, ratio=0.7)
+     
 
-    def jaccard_similarity(x, y):
-        pass
+    # TODO: What should be directory args.path_kg?
+    name_KG = args.path_kg.split('/')[-1].split('.')[0]
+
+    directory = f"incomplete_{name_KG}"
+    paths_of_incomplete_kgs = generated_incomplete_kg(kb_path=args.path_kg, directory=directory, n=1, ratio=0.99)
+
 
     expressions = None
 
+    
+
     for path_of_an_incomplete_kgs in paths_of_incomplete_kgs:
+
+        symbolic_kb = KnowledgeBase(path=path_of_an_incomplete_kgs)
+        # print({i.str for i in symbolic_kb.individuals(OWLClass(IRI('http://www.w3.org/2002/07/owl#','female')))})
+        # exit(0)
+
+        namespace = list(symbolic_kb.ontology.classes_in_signature())[0].iri.get_namespace()
+
+        parser = DLSyntaxParser(namespace)
+
+        list_jaccard_symbolic = []
+        list_jaccard_neural = []
 
         # Train a KGE, retrieval eval vs KGE and Symbolic
         # args.ratio_sample_nc
@@ -37,26 +91,54 @@ def execute(args):
         subprocess.run(['python', 'examples/retrieval_eval.py', "--path_kg", path_of_an_incomplete_kgs])
         # Load the results on the current view.
         df = pd.read_csv("ALCQHI_Retrieval_Results.csv", index_col=0)
+
         # Sanity checking
         if expressions is None:
-            expressions = df["Expressions"].values
+            expressions = df["Expression"].values
         else:
-            assert expressions == df["Expressions"].values
+            assert expressions == df["Expression"].values
 
         # Iterate
-        for row in df:
-            target_concept = row["Expression"]
+        for expression in expressions:
+
             # TODO: str -> owlapy.owl_classexpression object
+            
+            target_concept = parser.parse_expression(expression) #row["Expression"]
+            # print(target_concept)
+            
+            
             goal_retrieval = {i.str for i in symbolic_kb.individuals(target_concept)}
+
             result_symbolic: Set[str]
             result_neural_symbolic: Set[str]
 
-            jaccard_sim_symbolic = jaccard_similarity(row["Symbolic_Retrieval_Neural"], goal_retrieval)
+            result_symbolic = df[df["Expression"]==expression]["Symbolic_Retrieval"].apply(ast.literal_eval)
+            result_symbolic = result_symbolic.iloc[0] 
 
-            jaccard_sim_neural = jaccard_similarity(row["Symbolic_Retrieval_Neural"], goal_retrieval)
+            result_neural_symbolic = df[df["Expression"]==expression]["Symbolic_Retrieval_Neural"].apply(ast.literal_eval)
+            result_neural_symbolic = result_neural_symbolic.iloc[0]
+            
+
+            # jaccard_sim_symbolic = jaccard_similarity(row["Symbolic_Retrieval_Neural"], goal_retrieval)
+            jaccard_sim_symbolic = jaccard_similarity(result_symbolic, goal_retrieval)
+
+            # jaccard_sim_neural = jaccard_similarity(row["Symbolic_Retrieval_Neural"], goal_retrieval)
+            jaccard_sim_neural = jaccard_similarity(result_neural_symbolic, goal_retrieval)
+
+            #Update the Average
+            list_jaccard_neural.append(jaccard_sim_neural)
+            list_jaccard_symbolic.append(jaccard_sim_symbolic)
+
 
             # Ideally
-            jaccard_sim_neural > jaccard_sim_symbolic
+            # jaccard_sim_neural > jaccard_sim_symbolic
+        avg_jaccard_sym = sum(list_jaccard_symbolic)/len(list_jaccard_symbolic)
+        avg_jaccard_neural = sum(list_jaccard_neural)/len(list_jaccard_neural)
+
+        print("Average jaccard symbolic", avg_jaccard_sym)
+        print("Average Jaccard neural", avg_jaccard_neural)
+        print(list_jaccard_neural)
+        print(list_jaccard_symbolic)
 
 
 def get_default_arguments():
