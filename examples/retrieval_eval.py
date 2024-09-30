f2b886aafa643e732ada2c30e97ffdaf798cf03f
Caglar Demir <caglardemir8@gmail.com>
2024-09-30 15:33:50 +0200

    refactored

--- examples/retrieval_eval.py
+++ examples/retrieval_eval.py
@@ -1,4 +1,45 @@
-"""python examples/retrieval_eval.py"""
+"""$ python examples/retrieval_eval.py --path_kg KGs/Family/father.owl
+
+##################################################
+Description of generated Concepts
+NC denotes the named concepts   |NC|=3
+NNC denotes the negated named concepts  |NNC|=3
+|NC UNION NC|=9
+|NC Intersection NC|=9
+NC* denotes the union of named concepts and negated named concepts      |NC*|=6
+|NC* UNION NC*|=36
+|NC* Intersection NC*|=36
+|exist R* NC*|=12
+|forall R* NC*|=12
+|Max Cardinalities|=36
+|Min Cardinalities|=36
+|exist R* Nominals|=40
+##################################################
+
+Expression: ∃ hasChild⁻.{heinz , stefan , martin} | Jaccard Similarity:1.0000 | F1 :1.0000 | Runtime Benefits:-0.003:   0%|                                                                                 | 0/232 [00:00<?, ?it/s]Invalid IRI detected: Nec8a193431bd4ba6a57485770b54e72b, error: string index out of range
+Expression: male ⊔ (¬female) | Jaccard Similarity:1.0000 | F1 :1.0000 | Runtime Benefits:-0.002: 100%|███████████████████████████████████████████████████████████████████████████████████████████| 232/232 [00:00<00:00, 285.35it/s]
+Type
+OWLClass                    3
+OWLObjectAllValuesFrom     12
+OWLObjectComplementOf       3
+OWLObjectIntersectionOf    45
+OWLObjectMaxCardinality    36
+OWLObjectMinCardinality    36
+OWLObjectSomeValuesFrom    52
+OWLObjectUnionOf           45
+Name: Type, dtype: int64
+                         Jaccard Similarity   F1  Runtime Benefits
+Type
+OWLClass                                1.0  1.0         -0.001484
+OWLObjectAllValuesFrom                  1.0  1.0         -0.004071
+OWLObjectComplementOf                   1.0  1.0         -0.001592
+OWLObjectIntersectionOf                 1.0  1.0         -0.002914
+OWLObjectMaxCardinality                 1.0  1.0          0.000511
+OWLObjectMinCardinality                 1.0  1.0         -0.002798
+OWLObjectSomeValuesFrom                 1.0  1.0         -0.001701
+OWLObjectUnionOf                        1.0  1.0         -0.002939
+
+"""
 from ontolearn.owl_neural_reasoner import TripleStoreNeuralReasoner
 from ontolearn.knowledge_base import KnowledgeBase
 from ontolearn.triple_store import TripleStore
@@ -34,17 +75,13 @@ def execute(args):
         symbolic_kb = KnowledgeBase(path=args.path_kg)
     # (2) Initialize Neural OWL Reasoner.
     if args.path_kge_model:
-        neural_owl_reasoner = TripleStoreNeuralReasoner(
-            path_neural_embedding=args.path_kge_model, gamma=args.gamma
-        )
+        neural_owl_reasoner = TripleStoreNeuralReasoner(path_neural_embedding=args.path_kge_model, gamma=args.gamma)
     else:
-        neural_owl_reasoner = TripleStoreNeuralReasoner(
-            path_of_kb=args.path_kg, gamma=args.gamma
-        )
+        neural_owl_reasoner = TripleStoreNeuralReasoner(path_of_kb=args.path_kg, gamma=args.gamma)
     # Fix the random seed.
     random.seed(args.seed)
     ###################################################################
-    # GENERATE ALCQ CONCEPTS TO EVALUATE RETRIEVAL PERFORMANCES
+    # GENERATE DL CONCEPTS TO EVALUATE RETRIEVAL PERFORMANCES
     # (3) R: Extract object properties.
     object_properties = {i for i in symbolic_kb.get_object_properties()}
     # (3.1) Subsample if required.
@@ -62,48 +99,48 @@ def execute(args):
         nc = {i for i in random.sample(population=list(nc), k=max(1, int(len(nc) * args.ratio_sample_nc)))}
     # (7) NC⁻: Complement of NC.
     nnc = {i.get_object_complement_of() for i in nc}
-    # (8) UNNC: NC UNION NC⁻.
-    unnc = nc.union(nnc)
+    # (8) NC*: NC UNION NC⁻.
+    nc_star = nc.union(nnc)
     # (9) Retrieve 10 random Nominals.
-    nominals = set(random.sample(symbolic_kb.all_individuals_set(), 10))
-    # (10) All Combinations of 3 for Nominals.
-    nominal_combinations = set(
-        OWLObjectOneOf(combination)
-        for combination in itertools.combinations(nominals, 3)
-    )
+    if len(symbolic_kb.all_individuals_set())>args.num_nominals:
+        nominals = set(random.sample(symbolic_kb.all_individuals_set(), args.num_nominals))
+    else:
+        nominals = symbolic_kb.all_individuals_set()
+    # (10) All combinations of 3 for Nominals, e.g. {martin, heinz, markus}
+    nominal_combinations = set( OWLObjectOneOf(combination)for combination in itertools.combinations(nominals, 3))
     # (11) NC UNION NC.
     unions = concept_reducer(nc, opt=OWLObjectUnionOf)
     # (12) NC INTERSECTION NC.
     intersections = concept_reducer(nc, opt=OWLObjectIntersectionOf)
-    # (13) UNNC UNION UNNC.
-    unions_unnc = concept_reducer(unnc, opt=OWLObjectUnionOf)
-    # (14) UNNC INTERACTION UNNC.
-    intersections_unnc = concept_reducer(unnc, opt=OWLObjectIntersectionOf)
-    # (15) \exist r. C s.t. C \in UNNC and r \in R* .
-    exist_unnc = concept_reducer_properties(
-        concepts=unnc,
+    # (13) NC* UNION NC*.
+    unions_nc_star = concept_reducer(nc_star, opt=OWLObjectUnionOf)
+    # (14) NC* INTERACTION NC*.
+    intersections_nc_star = concept_reducer(nc_star, opt=OWLObjectIntersectionOf)
+    # (15) \exist r. C s.t. C \in NC* and r \in R* .
+    exist_nc_star = concept_reducer_properties(
+        concepts=nc_star,
         properties=object_properties_and_inverse,
         cls=OWLObjectSomeValuesFrom,
     )
-    # (16) \forall r. C s.t. C \in UNNC and r \in R* .
-    for_all_unnc = concept_reducer_properties(
-        concepts=unnc,
+    # (16) \forall r. C s.t. C \in NC* and r \in R* .
+    for_all_nc_star = concept_reducer_properties(
+        concepts=nc_star,
         properties=object_properties_and_inverse,
         cls=OWLObjectAllValuesFrom,
     )
-    # (17) >= n r. C  and =< n r. C, s.t. C \in UNNC and r \in R* .
-    min_cardinality_unnc_1, min_cardinality_unnc_2, min_cardinality_unnc_3 = (
+    # (17) >= n r. C  and =< n r. C, s.t. C \in NC* and r \in R* .
+    min_cardinality_nc_star_1, min_cardinality_nc_star_2, min_cardinality_nc_star_3 = (
         concept_reducer_properties(
-            concepts=unnc,
+            concepts=nc_star,
             properties=object_properties_and_inverse,
             cls=OWLObjectMinCardinality,
             cardinality=i,
         )
         for i in [1, 2, 3]
     )
-    max_cardinality_unnc_1, max_cardinality_unnc_2, max_cardinality_unnc_3 = (
+    max_cardinality_nc_star_1, max_cardinality_nc_star_2, max_cardinality_nc_star_3 = (
         concept_reducer_properties(
-            concepts=unnc,
+            concepts=nc_star,
             properties=object_properties_and_inverse,
             cls=OWLObjectMaxCardinality,
             cardinality=i,
@@ -120,7 +157,6 @@ def execute(args):
     ###################################################################
 
     # Retrieval Results
-
     def concept_retrieval(retriever_func, c) -> Tuple[Set[str], float]:
         start_time = time.time()
         return {i.str for i in retriever_func.individuals(c)}, time.time() - start_time
@@ -130,13 +166,36 @@ def execute(args):
     # () Converted to list so that the progress bar works.
     concepts = list(
         chain(
-            nc, unions, intersections, nnc, unnc, unions_unnc, intersections_unnc,
-            exist_unnc, for_all_unnc,
-            min_cardinality_unnc_1, min_cardinality_unnc_2, min_cardinality_unnc_3,
-            max_cardinality_unnc_1, max_cardinality_unnc_2, max_cardinality_unnc_3,
-            exist_nominals,
-        )
-    )
+            nc,           # named concepts          (C)
+            nnc,                   # negated named concepts  (\neg C)
+            unions,                # unions of named concepts ( A OR C)
+            intersections,         # intersection of named concepts
+            unions_nc_star,        # A set of Union of named concepts and negat
+            intersections_nc_star, #
+            exist_nc_star,
+            for_all_nc_star,
+            min_cardinality_nc_star_1, min_cardinality_nc_star_1, min_cardinality_nc_star_3,
+            max_cardinality_nc_star_1, max_cardinality_nc_star_2, max_cardinality_nc_star_3,
+            exist_nominals))
+    print("\n")
+    print("#"*50)
+    print("Description of generated Concepts")
+    print(f"NC denotes the named concepts\t|NC|={len(nc)}")
+    print(f"NNC denotes the negated named concepts\t|NNC|={len(nnc)}")
+    print(f"|NC UNION NC|={len(unions)}")
+    print(f"|NC Intersection NC|={len(intersections)}")
+
+    print(f"NC* denotes the union of named concepts and negated named concepts\t|NC*|={len(nc_star)}")
+    print(f"|NC* UNION NC*|={len(unions_nc_star)}")
+    print(f"|NC* Intersection NC*|={len(intersections_nc_star)}")
+    print(f"|exist R* NC*|={len(exist_nc_star)}")
+    print(f"|forall R* NC*|={len(for_all_nc_star)}")
+
+    print(f"|Max Cardinalities|={len(max_cardinality_nc_star_1) + len(max_cardinality_nc_star_2)+ len(max_cardinality_nc_star_3)}")
+    print(f"|Min Cardinalities|={len(min_cardinality_nc_star_1) + len(min_cardinality_nc_star_1)+ len(min_cardinality_nc_star_3)}")
+    print(f"|exist R* Nominals|={len(exist_nominals)}")
+    print("#" * 50,end="\n\n")
+
     # () Shuffled the data so that the progress bar is not influenced by the order of concepts.
     random.shuffle(concepts)
     # () Iterate over single OWL Class Expressions in ALCQIHO
@@ -198,6 +257,8 @@ def get_default_arguments():
     parser.add_argument("--seed", type=int, default=1)
     parser.add_argument("--ratio_sample_nc", type=float, default=None, help="To sample OWL Classes.")
     parser.add_argument("--ratio_sample_object_prob", type=float, default=None, help="To sample OWL Object Properties.")
+    parser.add_argument("--num_nominals", type=int, default=10, help="Number of OWL named individuals to be sampled.")
+
     # H is obtained if the forward chain is applied on KG.
     parser.add_argument("--path_report", type=str, default="ALCQHI_Retrieval_Results.csv")
     return parser.parse_args()
