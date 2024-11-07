2441b450d57fdc252cc16b24b02a5f3f91e0a779
Caglar Demir <caglardemir8@gmail.com>
2024-11-07 20:35:00 +0100

    Tests for CELOE and Evolearner example added

--- tests/test_evolearner.py
+++ tests/test_evolearner.py
@@ -9,6 +9,14 @@ from ontolearn.knowledge_base import KnowledgeBase
 from ontolearn.concept_learner import EvoLearner
 from ontolearn.utils import setup_logging
 
+import json
+import os
+from ontolearn.knowledge_base import KnowledgeBase
+from ontolearn.concept_learner import EvoLearner
+from ontolearn.learning_problem import PosNegLPStandard
+from owlapy.owl_individual import OWLNamedIndividual, IRI
+from owlapy.class_expression import OWLClass
+
 random.seed(1)
 
 
@@ -61,3 +69,56 @@ class TestEvoLearner(unittest.TestCase):
         returned_model = model.fit(learning_problem=lp)
         best_pred = returned_model.best_hypotheses(n=1, return_node=True)
         assert best_pred.quality == 1.00
+
+    def test_example(self):
+
+        with open('examples/synthetic_problems.json') as json_file:
+            settings = json.load(json_file)
+
+        kb = KnowledgeBase(path="KGs/Family/family-benchmark_rich_background.owl")
+
+        # noinspection DuplicatedCode
+        for str_target_concept, examples in settings['problems'].items():
+            p = set(examples['positive_examples'])
+            n = set(examples['negative_examples'])
+            print('Target concept: ', str_target_concept)
+
+            # lets inject more background info
+            if str_target_concept in ['Granddaughter', 'Aunt', 'Sister']:
+                NS = 'http://www.benchmark.org/family#'
+                concepts_to_ignore = {
+                    OWLClass(IRI(NS, 'Brother')),
+                    OWLClass(IRI(NS, 'Sister')),
+                    OWLClass(IRI(NS, 'Daughter')),
+                    OWLClass(IRI(NS, 'Mother')),
+                    OWLClass(IRI(NS, 'Grandmother')),
+                    OWLClass(IRI(NS, 'Father')),
+                    OWLClass(IRI(NS, 'Grandparent')),
+                    OWLClass(IRI(NS, 'PersonWithASibling')),
+                    OWLClass(IRI(NS, 'Granddaughter')),
+                    OWLClass(IRI(NS, 'Son')),
+                    OWLClass(IRI(NS, 'Child')),
+                    OWLClass(IRI(NS, 'Grandson')),
+                    OWLClass(IRI(NS, 'Grandfather')),
+                    OWLClass(IRI(NS, 'Grandchild')),
+                    OWLClass(IRI(NS, 'Parent')),
+                }
+                target_kb = kb.ignore_and_copy(ignored_classes=concepts_to_ignore)
+            else:
+                target_kb = kb
+
+            typed_pos = set(map(OWLNamedIndividual, map(IRI.create, p)))
+            typed_neg = set(map(OWLNamedIndividual, map(IRI.create, n)))
+            lp = PosNegLPStandard(pos=typed_pos, neg=typed_neg)
+
+            model = EvoLearner(knowledge_base=target_kb, max_runtime=600)
+            model.fit(lp, verbose=False)
+
+            model.save_best_hypothesis(n=3, path=f"Predictions_{str_target_concept}")
+            # Get Top n hypotheses
+            hypotheses = list(model.best_hypotheses(n=3))
+            # Use hypotheses as binary function to label individuals.
+            predictions = model.predict(individuals=list(typed_pos | typed_neg),
+                                        hypotheses=hypotheses)
+            # print(predictions)
+            [print(_) for _ in hypotheses]
