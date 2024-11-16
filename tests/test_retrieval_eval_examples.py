dfb0a4fd773a54a7288cfd253f299ec8df6e9c75
Alkid <alkid1baci@gmail.com>
2024-11-16 20:33:54 +0100

    clean directories before and after retrieval tests

--- tests/test_retrieval_eval_examples.py
+++ tests/test_retrieval_eval_examples.py
@@ -4,6 +4,16 @@ from examples.retrieval_eval import execute
 from examples.retrieval_eval_under_incomplete import execute as execute2
 import shutil
 
+
+def clear():
+    if os.path.exists("incomplete_father_0_1"):
+        shutil.rmtree("incomplete_father_0_1")
+    if os.path.exists("KGs_Family_father_owl"):
+        shutil.rmtree("KGs_Family_father_owl")
+    if os.path.exists("checkpoints"):
+        shutil.rmtree("checkpoints")
+
+
 class RetrievalTests(unittest.TestCase):
 
     def test_retrieval_eval(self):
@@ -20,6 +30,7 @@ class RetrievalTests(unittest.TestCase):
                 self.num_nominals = 10
                 self.path_report = "incomplete_father_0_1/ALCQHI_Retrieval_Results.csv"
         args = ARGS()
+        clear()
         os.mkdir("incomplete_father_0_1")
         js, f1 = execute(args)
 
@@ -43,10 +54,5 @@ class RetrievalTests(unittest.TestCase):
         results = execute2(args)
         for r, v in results.items():
             self.assertGreaterEqual(v, 0.9)
-        if os.path.exists("incomplete_father_0_1"):
-            shutil.rmtree("incomplete_father_0_1")
-        if os.path.exists("KGs_Family_father_owl"):
-            shutil.rmtree("KGs_Family_father_owl")
-        if os.path.exists("checkpoints"):
-            shutil.rmtree("checkpoints")
+        clear()
 
