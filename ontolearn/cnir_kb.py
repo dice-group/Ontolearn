d5257b3e13d4fe6f167ff83779b9276b4caa4912
Jean-KOUAGOU <jeank@aims.ac.za>
2025-04-11 12:48:44 +0200

    update CNIRKB

--- ontolearn/cnir_kb.py
+++ ontolearn/cnir_kb.py
@@ -25,45 +25,61 @@
 """ CNIR Knowledge Base."""
 from ontolearn.knowledge_base import KnowledgeBase
 import time
-
+from typing import Iterable, Optional, Callable, Union, FrozenSet, Set, Dict, cast, Generator
+from owlapy.abstracts import AbstractOWLOntology, AbstractOWLReasoner
+from owlapy.owl_hierarchy import ClassHierarchy, ObjectPropertyHierarchy, DatatypePropertyHierarchy
+from owlapy.owl_individual import OWLNamedIndividual
 import numpy as np
 import torch
 from transformers import AutoModel, AutoTokenizer, AutoConfig
 
 from owlapy.render import DLSyntaxObjectRenderer
 from owlapy.parser import DLSyntaxParser
+from owlapy.utils import iter_count
 
-from cnir import InferenceDataset
-from cnir.config import CNIRConfig
-from cnir.models import CNIRComposite, CNIRTransformer, CNIRLSTM, CNIRGRU
-from cnir.models.pmanet import PMAnet
-from cnir.utils import str2bool, read_embs_and_apply_agg
-from cnir.utils import score_all_inds, score_all_inds_composite
+from .cnir import InferenceDataset
+from .cnir.config import CNIRConfig
+from .cnir.models import CNIRComposite, CNIRTransformer, CNIRLSTM, CNIRGRU
+from .cnir.models.pmanet import PMAnet
+from .cnir.utils import str2bool, read_embs_and_apply_agg
+from .cnir.utils import score_all_inds, score_all_inds_composite
 
 
 class CNIRKB(KnowledgeBase):
-    def __init__(self, dataset_dir, model, model_path, use_pma, pma_model_path, tokenizer_path=None,
+    def __init__(self, 
+                 path: Optional[str] = None,
+                 reasoner_factory: Optional[Callable[[AbstractOWLOntology], AbstractOWLReasoner]] = None,
+                 ontology: Optional[AbstractOWLOntology] = None,
+                 reasoner: Optional[AbstractOWLReasoner] = None,
+                 class_hierarchy: Optional[ClassHierarchy] = None,
+                 load_class_hierarchy: bool = True,
+                 object_property_hierarchy: Optional[ObjectPropertyHierarchy] = None,
+                 data_property_hierarchy: Optional[DatatypePropertyHierarchy] = None,
+                 include_implicit_individuals=False,
+                 dataset_dir=None, model=None, model_path=None, use_pma=True, pma_model_path=None, tokenizer_path=None,
                  chunksize = 1024, th=0.5):
+        
         self.th = th
         self.pma_net = None
         self.model_name = model
         self.chunksize = chunksize
-
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        
         if model.lower() == "composite" and use_pma:
             self.pma_net = PMAnet(CNIRConfig().embedding_dim, CNIRConfig().num_attention_heads, 1)
             self.pma_net.load_state_dict(
                 torch.load(pma_model_path, map_location="cpu", weights_only=True)
             )
             self.pma_net.eval()
-
+        
         self.kb, self.all_individuals, self.embeddings = read_embs_and_apply_agg(
             dataset_dir, nn_agg=self.pma_net, merge=True
         )
         self.dls_renderer = DLSyntaxObjectRenderer()
         self.all_individuals_set = set(self.all_individuals)
         self.all_individuals_arr = np.array(sorted(self.all_individuals), dtype=object)
-        self.all_ind_embs = torch.FloatTensor(self.embeddings.loc[self.all_individuals_arr].values)
-
+        self.all_ind_embs = torch.FloatTensor(self.embeddings.loc[self.all_individuals_arr].values).to(self.device)
+        
         kb_namespace = list(self.kb.ontology.classes_in_signature())[0].str
         if "#" in kb_namespace:
             self.kb_namespace = kb_namespace.split("#")[0] + "#"
@@ -74,7 +90,6 @@ class CNIRKB(KnowledgeBase):
         else:
             self.kb_namespace = kb_namespace
         self.expression_parser = DLSyntaxParser(self.kb_namespace)
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         AutoConfig.register("cnir", CNIRConfig)
         if model.lower() == "composite":
             AutoModel.register(CNIRConfig, CNIRComposite)
@@ -84,11 +99,21 @@ class CNIRKB(KnowledgeBase):
             AutoModel.register(CNIRConfig, CNIRGRU)
         elif model.lower() == "transformer":
             AutoModel.register(CNIRConfig, CNIRTransformer)
-        self.model = AutoModel.from_pretrained(model_path)
+
+        print("\n"+"\x1b[0;30;43m"+"Loading Model..."+"\x1b[0m"+"\n")
+        self.model = AutoModel.from_pretrained(model_path, device_map="auto")
         if model.lower() != "composite":
             self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
-        AutoConfig.register("cnir", CNIRConfig)
-        AutoModel.register(CNIRConfig, CNIRComposite)
+
+        super().__init__(path=path,
+                     reasoner_factory=reasoner_factory,
+                     ontology=ontology,
+                     reasoner=reasoner,
+                     class_hierarchy=class_hierarchy,
+                     load_class_hierarchy=load_class_hierarchy,
+                     object_property_hierarchy=object_property_hierarchy,
+                     data_property_hierarchy=data_property_hierarchy,
+                     include_implicit_individuals=include_implicit_individuals)
 
     def cnir_predict(self, expr):
         start_time = time.time()
@@ -125,26 +150,32 @@ class CNIRKB(KnowledgeBase):
         retrieved = set(self.all_individuals_arr[np.where(outputs > self.th)[0]])
         time_taken = time.time() - start_time
         return retrieved, time_taken
-    def individuals(self, concept):
-        expr = concept if isinstance(concept, str) else self.dls_renderer.render(concept)
-        if self.model_name.lower() == "composite":
-            individuals, _ = self.cnir_predict(expr)
+        
+    def individuals(self, concept=None, named_individuals=False):
+        if isinstance(concept, str):
+            expr = concept
+        elif concept is None:
+            expr = '⊤'
         else:
-            individuals, _ = self.cnir_encoders_predict(expr)
+            expr = self.dls_renderer.render(concept)
+        try:
+            if self.model_name.lower() == "composite":
+                individuals, _ = self.cnir_predict(expr)
+            else:
+                individuals, _ = self.cnir_encoders_predict(expr)
+        except:
+            individuals, _ = self.cnir_predict('⊤')
         # represent the individuals in kb namespace format
         namespace = self.kb_namespace.split("/", 3)
         namespace = "/".join(namespace[:3]) + "/"
-        individuals = {namespace+i for i in individuals}
-
+        individuals = frozenset(list(map(lambda x: OWLNamedIndividual(namespace+x), individuals)))
         return individuals
 
+    def __repr__(self):
+        properties_count = iter_count(self.ontology.object_properties_in_signature()) + iter_count(
+            self.ontology.data_properties_in_signature())
+        class_count = iter_count(self.ontology.classes_in_signature())
+        individuals_count = self.individuals_count()
 
-if __name__ == "__main__":
-    data_dir = "cnir/mutagenesis"
-    model = "composite"
-    model_path = "cnir/pretrained_model/composite"
-    use_pma = True
-    pma_model_path = "cnir/pretrained_model/pma.pt"
-    th = 0.5
-    cnir_kb = CNIRKB(data_dir, model, model_path, use_pma, pma_model_path, th)
-    print(cnir_kb.individuals("Carbon-29"))
+        return f'CNIRKB(path={repr(self.path)} [model={repr(self.model_name.upper())}] <{class_count} classes, {properties_count} properties, ' \
+               f'{individuals_count} individuals)'
