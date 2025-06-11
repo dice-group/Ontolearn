799792e22b70e33b43049c905af01fe0335a12f3
moreshud <mo.yekini@gmail.com>
2025-06-11 15:18:19 +0200

    fix: add openai to setup.py

--- setup.py
+++ setup.py
@@ -59,7 +59,8 @@ _deps = [
     "myst-parser>=2.0.0",
     "flake8>=6.0.0",
     "fastapi>=0.110.1",
-    "uvicorn>=0.29.0"]
+    "uvicorn>=0.29.0",
+    "openai>=1.86.0"]
 
 deps = {b: a for a, b in (re.findall(r"^(([^!=<>~ ]+)(?:[!=<>~ ].*)?$)", x)[0] for x in _deps)}
 
