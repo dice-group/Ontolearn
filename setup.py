83c5748aed848af6a323f7b22a5120fff3a17bec
alkidbaci <alkid1baci@gmail.com>
2026-02-05 12:28:42 +0100

    fixed dependency name

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ extras["min"] = deps_list(
 
 extras["doc"] = (extras["min"] + deps_list("sphinx",
                            "sphinx-autoapi",
-                           "sphinx_theme",
+                           "sphinx-theme",
                            "sphinx_rtd_theme",
                            "sphinxcontrib-plantuml",
                            "plantuml-local-client", "myst-parser"))
